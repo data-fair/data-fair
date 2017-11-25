10574c076761087441cd7d624bcdccfa9ece0182
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2017-11-25 08:55:17 +0100

    kid to use should be in JWT header

--- config/default.js
+++ config/default.js
@@ -2,7 +2,6 @@ module.exports = {
   port: 5600,
   baseUrl: 'http://localhost:5601',
   koumoulUrl: 'https://staging.koumoul.com',
-  koumoulKid: 'koumoul-staging',
   dataDir: './data',
   mongoUrl: 'mongodb://localhost:27017/accessible-data-' + (process.env.NODE_ENV || 'development')
 }
