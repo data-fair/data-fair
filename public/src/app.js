54a5c6e739a1597d91f352e220ec87c07511369a
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2017-11-26 16:15:57 +0100

    feat: WIP on dataset page

--- public/src/app.js
+++ public/src/app.js
@@ -14,6 +14,7 @@ import App from './App.vue'
 Vue.use(VueMaterial)
 Vue.use(VueResource)
 Vue.use(VueRouter)
+Vue.use(require('vue-moment'))
 // Vue.use(VueCookie)
 
 Vue.material.registerTheme('default', {
--- public/src/pages/Dataset.vue
+++ public/src/pages/Dataset.vue
@@ -1,20 +1,51 @@
 <template>
 <md-layout md-align="center">
   <md-layout md-column md-flex="90" v-if="dataset">
-    <md-layout md-row>
-      <md-layout md-column md-flex="40">
-        <md-input-container>
-          <label>Titre</label>
-          <md-input v-model="dataset.title" @blur="save"></md-input>
-        </md-input-container>
-      </md-layout>
-      <md-layout md-column md-flex="55" md-flex-offset="5">
-        <md-input-container>
-          <label>Titre</label>
-          <md-textarea v-model="dataset.description" @blur="save"></md-textarea>
-        </md-input-container>
-      </md-layout>
-    </md-layout>
+    <md-tabs md-fixed class="md-transparent">
+      <md-tab md-label="Métadonnées" md-icon="toc">
+        <md-layout md-row>
+          <md-layout md-column md-flex="50">
+            <md-input-container>
+              <label>Titre</label>
+              <md-input v-model="dataset.title" @blur="save"></md-input>
+            </md-input-container>
+            <md-input-container>
+              <label>Description</label>
+              <md-textarea v-model="dataset.description" @blur="save"></md-textarea>
+            </md-input-container>
+          </md-layout>
+          <md-layout md-column md-flex="40" md-flex-offset="10">
+            <md-card>
+              <md-list>
+                <md-list-item>
+                  <md-icon>insert_drive_file</md-icon> <span>{{dataset.originalFileName}}</span> <span>{{(dataset.fileSize / 1024).toFixed(2)}} ko</span>
+                </md-list-item>
+                <md-list-item>
+                  <md-icon>update</md-icon> <span>{{dataset.updatedBy}}</span> <span>{{dataset.updatedAt | moment("DD/MM/YYYY, HH:mm")}}</span>
+                </md-list-item>
+                <md-list-item>
+                  <md-icon>add_circle_outline</md-icon> <span>{{dataset.createdBy}}</span> <span>{{dataset.createdAt | moment("DD/MM/YYYY, HH:mm")}}</span>
+                </md-list-item>
+              </md-list>
+            </md-card>
+          </md-layout>
+        </md-layout>
+      </md-tab>
+
+      <md-tab md-label="Vue tableau" md-icon="view_list">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
+      </md-tab>
+
+      <md-tab md-label="Permissions" md-icon="lock">
+        <permissions :dataset="dataset" @change="save"></permissions>
+      </md-tab>
+
+
+
+      <md-tab md-label="Journal" md-icon="event_note">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
+      </md-tab>
+    </md-tabs>
   </md-layout>
 </md-layout>
 </template>
@@ -23,9 +54,13 @@
 const {
   mapState
 } = require('vuex')
+import Permissions from '../components/Permissions.vue'
 
 export default {
   name: 'dataset',
+  components: {
+    Permissions
+  },
   data: () => ({
     dataset: null
   }),
