74228fb76ac0ef52d6c9f0c5733e17ff3c3e414d
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2017-11-26 16:15:30 +0100

    feat(UI): WIP on permissions

---
+++ public/src/components/Permissions.vue
@@ -0,0 +1,123 @@
+<template>
+<div>
+  <h3 class="md-headline">Visibilité</h3>
+  <md-switch v-model="dataset.public" @change="$emit('change')">Public (visible par tous)</md-switch>
+  <h3 class="md-headline">Permissions</h3>
+  <md-table>
+    <md-table-header>
+      <md-table-row>
+        <md-table-head>Personne ou organisation</md-table-head>
+        <md-table-head>Nom</md-table-head>
+        <md-table-head>Mode</md-table-head>
+        <!-- <md-table-head>
+          <md-input-container>
+            <label for="movie">Movie</label>
+            <md-select name="movie" id="movie" v-model="movie">
+              <md-option value="fight_club">Fight Club</md-option>
+              <md-option value="godfather">Godfather</md-option>
+              <md-option value="godfather_ii">Godfather II</md-option>
+              <md-option value="godfather_iii">Godfather III</md-option>
+              <md-option value="godfellas">Godfellas</md-option>
+              <md-option value="pulp_fiction">Pulp Fiction</md-option>
+              <md-option value="scarface">Scarface</md-option>
+            </md-select>
+          </md-input-container>
+        </md-table-head> -->
+      </md-table-row>
+    </md-table-header>
+
+    <md-table-body>
+      <md-table-row>
+        <md-table-cell>
+          <md-select v-model="newPermission.type">
+            <md-option value="organization">Organisation</md-option>
+            <md-option value="user">Personne</md-option>
+          </md-select>
+        </md-table-cell>
+        <md-table-cell>
+          <md-select v-model="newPermission.id">
+            <md-option value="organization">Organisation</md-option>
+            <md-option value="user">Personne</md-option>
+          </md-select>
+        </md-table-cell>
+        <md-table-cell>
+          <md-select v-model="newPermission.mode">
+            <md-option value="read">Lecture</md-option>
+            <md-option value="write">Lecture / Ecriture</md-option>
+          </md-select>
+        </md-table-cell>
+        <md-table-cell>
+          <md-button class="md-icon-button md-raised md-primary">
+            <md-icon>add</md-icon>
+          </md-button>
+        </md-table-cell>
+      </md-table-row>
+      <md-table-row v-for="(permission, rowIndex) in dataset.permissions" :key="rowIndex">
+        <md-table-cell>
+          <md-select v-model="permission.type">
+            <md-option value="organization">Organisation</md-option>
+            <md-option value="user">Personne</md-option>
+          </md-select>
+        </md-table-cell>
+        <md-table-cell>
+          <md-select v-model="permission.id">
+            <md-option value="organization">Organisation</md-option>
+            <md-option value="user">Personne</md-option>
+          </md-select>
+        </md-table-cell>
+        <md-table-cell>
+          <md-select v-model="permission.mode">
+            <md-option value="read">Lecture</md-option>
+            <md-option value="write">Lecture / Ecriture</md-option>
+          </md-select>
+        </md-table-cell>
+        <md-table-cell>
+          <md-button class="md-icon-button md-raised md-warn">
+            <md-icon>remove</md-icon>
+          </md-button>
+        </md-table-cell>
+      </md-table-row>
+    </md-table-body>
+  </md-table>
+  <h3 class="md-headline">Transférer la propriété</h3>
+
+</div>
+</template>
+
+<script>
+const {
+  mapState
+} = require('vuex')
+
+export default {
+  name: 'permissions',
+  props: ['dataset'],
+  data: () => ({
+    newPermission: {
+      type: 'organization',
+      id: null,
+      mode: 'read'
+    },
+    organizations: {},
+    users: {}
+  }),
+  mounted() {
+    this.dataset.permissions = this.dataset.permissions || []
+    this.$http.get(window.CONFIG.directoryUrl + '/api/organizations').then(results => {
+      this.organizations = Object.assign({}, ...results.data.results.map(organization => ({
+        [organization.id]: organization
+      })))
+    })
+    this.$http.get(window.CONFIG.directoryUrl + '/api/users').then(results => {
+      this.users = Object.assign({}, ...results.data.results.map(user => ({
+        [user.id]: user
+      })))
+    })
+  },
+  watch: {
+    dataset() {
+      this.dataset.permissions = this.dataset.permissions || []
+    }
+  }
+}
+</script>
