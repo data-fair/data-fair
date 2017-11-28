600e8000270555e0bf08c80ecb5f634f463e8910
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2017-11-28 11:04:40 +0100

    feat: workers skeleton

---
+++ server/workers/indexer.js
@@ -0,0 +1,5 @@
+// Index tabular datasets with elasticsearch using available information on dataset schema
+
+const indexDatasets = module.exports = async function(db, es) {
+  setTimeout(() => indexDatasets(db, es), 1000)
+}
---
+++ server/workers/schema-analyzer.js
@@ -0,0 +1,5 @@
+// Analyze dataset data and try to guess the schém
+
+const analyzeDataset = module.exports = async function(db, es) {
+  setTimeout(() => analyzeDataset(db, es), 1000)
+}
