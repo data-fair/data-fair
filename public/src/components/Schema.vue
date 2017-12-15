dc3f3ee7623f99d8580d236a977ee1c216495daa
Alban Mouton <alban.mouton@gmail.com>
2017-12-15 12:17:01 +0100

    feat: better form management on schema edition

--- public/src/components/Schema.vue
+++ public/src/components/Schema.vue
@@ -1,43 +1,44 @@
 <template>
 <md-layout md-column>
-  <md-layout md-row>
-    <md-layout md-column md-flex="60">
-      <h3 class="md-headline">Schéma</h3>
+  <form @submit.prevent="updateSchema">
+    <md-layout md-row>
+      <h3 class="md-headline" style="flex:1">Schéma</h3>
+      <div style="margin-top: 24px;">
+        <md-button type="submit" class="md-raised md-warn" v-if="updated">Appliquer</md-button>
+        <md-button v-if="updated" @click="resetSchema">Annuler les modifications</md-button>
+      </div>
     </md-layout>
-    <md-layout md-column>
-      <md-button class="md-raised" v-if="updated" @click="resetSchema" style="position:absolute;">Annuler les modifications</md-button>
-      <md-button class="md-raised md-warn" v-if="updated" @click="updateSchema" style="position:absolute;margin-left:260px;">Appliquer</md-button>
-    </md-layout>
-  </md-layout>
-  <md-list>
-    <md-list-item v-for="field in schema">
-      <md-layout md-row>
-        <md-layout md-flex="20">
-          <md-input-container>
-            <label>Libellé</label>
-            <md-input v-model="field.title" :placeholder="field['x-originalName']"></md-input>
-          </md-input-container>
-        </md-layout>
 
-        <md-layout md-flex="45" md-flex-offset="5">
-          <md-input-container>
-            <label>Description</label>
-            <md-textarea v-model="field.description"></md-textarea>
-          </md-input-container>
-        </md-layout>
+    <md-list>
+      <md-list-item v-for="field in schema">
+        <md-layout md-row>
+          <md-layout md-flex="20">
+            <md-input-container>
+              <label>Libellé</label>
+              <md-input v-model="field.title" :placeholder="field['x-originalName']"></md-input>
+            </md-input-container>
+          </md-layout>
 
-        <md-layout md-flex="25" md-flex-offset="5">
-          <md-input-container>
-            <label>Concept</label>
-            <md-select v-model="field['x-refersTo']">
-              <md-option :value="null">Pas de concept</md-option>
-              <md-option :value="term.identifiers[0]" v-for="term in vocabulary">{{term.title}}</md-option>
-            </md-select>
-          </md-input-container>
+          <md-layout md-flex="45" md-flex-offset="5">
+            <md-input-container>
+              <label>Description</label>
+              <md-textarea v-model="field.description"></md-textarea>
+            </md-input-container>
+          </md-layout>
+
+          <md-layout md-flex="25" md-flex-offset="5">
+            <md-input-container>
+              <label>Concept</label>
+              <md-select v-model="field['x-refersTo']">
+                <md-option :value="null">Pas de concept</md-option>
+                <md-option :value="term.identifiers[0]" v-for="term in vocabulary">{{term.title}}</md-option>
+              </md-select>
+            </md-input-container>
+          </md-layout>
         </md-layout>
-      </md-layout>
-    </md-list-item>
-  </md-list>
+      </md-list-item>
+    </md-list>
+  </form>
 </md-layout>
 </template>
 
