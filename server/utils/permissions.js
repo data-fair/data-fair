20ebd6945899b3503f408ab51170e22662ff2df3
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2017-12-20 10:40:27 +0100

    fix: error when API has no permissions array

--- server/utils/permissions.js
+++ server/utils/permissions.js
@@ -1,7 +1,7 @@
 // TODO : replace api with a name that match external-api and dataset
 module.exports = function(api, operationId, user) {
   if (!user) {
-    const operationPermissions = api.permissions.filter(p => p.operationId === operationId)
+    const operationPermissions = (api.permissions || []).filter(p => p.operationId === operationId)
     // check if the operation is public
     if (operationPermissions.find(p => !p.type && !p.id)) return true
     return false
@@ -10,7 +10,7 @@ module.exports = function(api, operationId, user) {
     if ((api.owner.type === 'user' && api.owner.id === user.id) || (api.owner.type === 'organization' && user.organizations.find(o => o.id === api.owner.id))) {
       return true
     } else {
-      const operationPermissions = api.permissions.filter(p => p.operationId === operationId)
+      const operationPermissions = (api.permissions || []).filter(p => p.operationId === operationId)
       if (operationPermissions.find(p => (p.type === 'user' && p.id === user.id) || (p.type === 'organization' && user.organizations.find(o => o.id === p.id)))) return true
       return false
     }
