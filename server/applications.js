38b670ddbdd4d365d96bb98c6fe9659438bf2a92
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2018-01-03 17:38:38 +0100

    fix: permissions on application proxy endpoint

--- server/applications.js
+++ server/applications.js
@@ -2,17 +2,18 @@ const express = require('express')
 const requestProxy = require('express-request-proxy')
 const config = require('config')
 const permissions = require('./utils/permissions')
+const auth = require('./auth')
 
 const router = module.exports = express.Router()
 
 // Proxy for applications
-router.get('/:applicationConfigId*', async function(req, res, next) {
+router.get('/:applicationConfigId*', auth.optionalJwtMiddleware, async function(req, res, next) {
   try {
     const applicationConfig = await req.app.get('db').collection('application-configs').findOne({
       id: req.params.applicationConfigId
     })
     if (!applicationConfig) return res.status(404).send('No application configured for this id')
-    if (!permissions.can(req.applicationConfig, 'useApplication', req.user)) return res.sendStatus(403)
+    if (!permissions.can(applicationConfig, 'useApplication', req.user)) return res.sendStatus(403)
     const options = {
       url: applicationConfig.url,
       headers: {
