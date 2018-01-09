467e46ddc2f0719d64f5eb55eec7247c05062b92
Alban Mouton <alban.mouton@gmail.com>
2018-01-09 16:48:58 +0100

    chore: dev infos-parcelles applications

--- config/development.js
+++ config/development.js
@@ -2,5 +2,14 @@ module.exports = {
   defaultLimits: {
     totalStorage: 1024 * 1024 * 1024,
     datasetStorage: -1
-  }
+  },
+  applications: [{
+    title: 'Infos parcelles',
+    description: 'Cette application permet de visualiser des informations additionnelles sur certaines parcelles et de les mettre en évidence.',
+    href: 'https://staging.koumoul.com/s/infos-parcelles/'
+  }, {
+    title: 'Infos parcelles (dev)',
+    description: 'Cette application permet de visualiser des informations additionnelles sur certaines parcelles et de les mettre en évidence.',
+    href: 'http://localhost:5801/'
+  }]
 }
