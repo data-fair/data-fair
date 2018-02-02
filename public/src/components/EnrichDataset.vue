20fdf23e7a26fc7acc5f35f183d5ffa2622662cf
Alban Mouton <alban.mouton@gmail.com>
2018-02-02 16:39:45 +0100

    feat: WIP on extension UI

--- public/src/components/EnrichDataset.vue
+++ public/src/components/EnrichDataset.vue
@@ -1,13 +1,18 @@
 <template lang="html">
-  <md-list v-if="vocabulary">
+  <md-layout md-column>
+    <p>Les actions d'enrichissement proposées ci-dessous dépendent des concepts associés à ce jeu de données et des services distants configurés.</p>
 
-    <p>Les actions d'enrichissement proposées ci-dessous dépendent des concepts associés aux colonnes de ce jeu de données et des servuces distants configurés.</p>
-    <md-list-item v-for="(action, i) in actions" :key="i">
-      <span>
-        <md-checkbox v-model="action.selected" class="md-primary"/>
-      </span>
-      <span>{{ action.summary }} (service {{ action.remoteService.title }})</span>
-      <!--<md-layout md-row style="padding:8px" md-vertical-align="center">
+    <md-layout>
+      <md-list v-if="vocabulary">
+        <md-list-item v-for="(extension, i) in dataset.extensions" :key="i">
+          <span>
+            <md-checkbox v-model="extension.active" class="md-primary"/>
+          </span>
+          <span>{{ remoteServices[extension.remoteService].actions[extension.action].summary }} (service {{ remoteServices[extension.remoteService].title }})</span>
+          <div style="position:absolute;bottom:0;left:0;right:0;" v-if="extension.progress !== undefined">
+            <md-progress :md-progress="100 * (extension.progress || 0)"/>
+          </div>
+          <!--<md-layout md-row style="padding:8px" md-vertical-align="center">
         <md-layout md-column md-flex="10">
           <h5 class="md-title">Utilisez</h5>
         </md-layout>
@@ -29,9 +34,13 @@
         </md-layout>
       </md-layout>
     -->
-      <md-divider class="md-inset"/>
-    </md-list-item>
-  </md-list>
+        </md-list-item>
+      </md-list>
+    </md-layout>
+    <div>
+      <md-button @click="save" class="md-raised md-warn">Appliquer</md-button>
+    </div>
+  </md-layout>
 </template>
 
 <script>
@@ -41,41 +50,67 @@ export default {
   props: ['dataset'],
   data() {
     return {
-      actions: []
+      actions: [],
+      remoteServices: {}
     }
   },
   computed: {
     ...mapState(['vocabulary']),
+    resourceUrl() {
+      return window.CONFIG.publicUrl + '/api/v1/datasets/' + this.dataset.id
+    },
     concepts() {
       return new Set(this.dataset.schema.map(field => field['x-refersTo']).filter(c => c))
     }
   },
   watch: {
     concepts() {
-      this.fetchActions()
+      this.fetchRemoteServices()
     }
   },
   mounted() {
     this.$store.dispatch('fetchVocabulary')
-    this.fetchActions()
+    this.fetchRemoteServices()
   },
   methods: {
     // An action is using an remote services endpoint to enrich a specific dataset
-    execute(action) {
-      const params = {
-        inputConcepts: action.input.filter(i => i.selected).map(i => i.concept),
-        dataset: this.dataset.id
-      }
-      this.$http.post(window.CONFIG.publicUrl + '/api/v1/remote-services/' + action.api.id + '/actions/' + action.id, params)
+    save() {
+      const patch = { extensions: this.dataset.extensions }
+      this.$http.patch(this.resourceUrl, patch).then(result => {
+        this.$store.dispatch('notify', `Le jeu de données a bien été mis à jour`)
+      }, error => {
+        this.$store.dispatch('notifyError', `Erreur ${error.status} pendant la mise à jour du jeu de données`)
+      })
     },
-    fetchActions() {
+    fetchRemoteServices() {
       if (this.concepts.size) {
         this.$http.get(window.CONFIG.publicUrl + '/api/v1/remote-services?input-concepts=' + [...this.concepts].map(encodeURIComponent).join(',')).then(res => {
-          res.data.results.forEach(r => r.actions.forEach(a => { a.remoteService = r }))
-          this.actions = [].concat(...res.data.results.map(r => r.actions.filter(a => a.input.map(i => i.concept).filter(x => this.concepts.has(x)).length))).filter(a => a.inputCollection && a.outputCollection)
+          this.remoteServices = {}
+          res.data.results
+            .filter(s => s.owner.type === this.dataset.owner.type && s.owner.id === this.dataset.owner.id)
+            .forEach(s => {
+              s.actions = s.actions
+                .filter(a => a.inputCollection && a.outputCollection)
+                .filter(a => a.input.find(i => this.concepts.has(i.concept)))
+                .reduce((a, b) => { a[b.id] = b; return a }, {})
+              this.remoteServices[s.id] = s
+            })
+
+          // Add/remove available extensions
+          const extensions = (this.dataset.extensions || []).filter(e => {
+            return this.remoteServices[e.remoteService] && this.remoteServices[e.remoteService].actions[e.action]
+          })
+          Object.keys(this.remoteServices).forEach(s => {
+            Object.keys(this.remoteServices[s].actions).forEach(a => {
+              if (!extensions.find(e => e.remoteService === s && e.action === a)) {
+                extensions.push({remoteService: s, action: a, active: false})
+              }
+            })
+          })
+          this.$set(this.dataset, 'extensions', extensions)
         })
       } else {
-        this.actions = []
+        this.remoteServices = {}
       }
     }
   }
