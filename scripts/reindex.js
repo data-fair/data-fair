9a32e1e986a57ba3d967694d78ada173bac7b579
Alban Mouton <alban.mouton@gmail.com>
2018-02-19 20:29:00 +0100

    feat: script to reinit statuses to 'schematized'

---
+++ scripts/reindex.js
@@ -0,0 +1,8 @@
+const dbUtils = require('../server/utils/db')
+
+async function main() {
+  const db = await dbUtils.init()
+  await db.collection('datasets').updateMany({}, {$set: {status: 'schematized'}})
+}
+
+main().then(() => process.exit(), err => { throw err })
