40932b2e27210b54184c589ac4d6b2debeda9ca2
Alban Mouton <alban.mouton@gmail.com>
2018-04-19 22:43:11 +0200

    chore: add reindex-errors.js script

--- scripts/reindex-errors.js
+++ scripts/reindex-errors.js
@@ -2,7 +2,7 @@ const dbUtils = require('../server/utils/db')
 
 async function main() {
   const db = await dbUtils.init()
-  await db.collection('datasets').updateMany({}, {$set: {status: 'schematized'}})
+  await db.collection('datasets').updateMany({status: 'error'}, {$set: {status: 'schematized'}})
 }
 
 main().then(() => process.exit())
