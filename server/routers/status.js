2f7dce4f57cb99237f9082defeb74231f3b14bda
Alban Mouton <alban.mouton@gmail.com>
2018-04-19 09:24:26 +0200

    feat: mongo, ES and JWKS verifications in status

--- server/routers/status.js
+++ server/routers/status.js
@@ -1,22 +1,53 @@
-function getStatus(callback) {
-  callback(null, {
-    status: 'ok',
-    message: 'Service is ok',
-    details: 'Service is ok'
-  })
+const config = require('config')
+const moment = require('moment')
+const rp = require('request-promise-native')
+const asyncWrap = require('../utils/async-wrap')
+
+async function mongoStatus(req) {
+  await req.app.get('db').admin().serverStatus()
 }
 
-exports.status = (req, res, next) => {
-  getStatus((err, status) => {
-    if (err) return next(err)
-    res.send(status)
-  })
+async function esStatus(req) {
+  await req.app.get('es').ping()
 }
 
-exports.ping = (req, res, next) => {
-  getStatus((err, status) => {
-    if (err) return next(err)
-    if (status.status === 'error') res.status(500).send(status)
-    else res.send(status.status)
-  })
+async function jwksStatus(req) {
+  const jwksRes = await rp.get({url: config.directoryUrl + '/.well-known/jwks.json', json: true})
+  if (!jwksRes || !jwksRes.keys || !jwksRes.keys.length) throw new Error('Incomplete JWKS response')
 }
+
+async function singleStatus(req, fn, name) {
+  let time = moment()
+  try {
+    await fn(req)
+    return {status: 'ok', name, timeInMs: moment().diff(time)}
+  } catch (err) {
+    return {status: 'error', message: err.toString(), name, timeInMs: moment().diff(time)}
+  }
+}
+
+async function getStatus(req) {
+  let results
+  results = await Promise.all([
+    singleStatus(req, mongoStatus, 'mongodb'),
+    singleStatus(req, esStatus, 'elasticsearch'),
+    singleStatus(req, jwksStatus, 'auth-directory')
+  ])
+  let errors = results.filter(r => r.status === 'error')
+  return {
+    status: errors.length ? 'error' : 'ok',
+    message: errors.length ? ('Problem with : ' + errors.map(s => s.name).join(', ')) : 'Service is ok',
+    details: results
+  }
+}
+
+exports.status = asyncWrap(async (req, res, next) => {
+  const status = await getStatus(req)
+  res.send(status)
+})
+
+exports.ping = asyncWrap(async(req, res, next) => {
+  const status = await getStatus(req)
+  if (status.status === 'error') res.status(500).send(status)
+  else res.send(status.status)
+})
