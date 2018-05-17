2d43d118477e30b959833ea5db919a03bb341e94
Alban Mouton <alban.mouton@gmail.com>
2018-05-17 13:59:55 +0200

    fix: do not expose only GET requests of applications

--- server/routers/application-proxy.js
+++ server/routers/application-proxy.js
@@ -6,7 +6,7 @@ const asyncWrap = require('../utils/async-wrap')
 const router = module.exports = express.Router()
 
 // Proxy for applications
-router.get('/:applicationId*', asyncWrap(async(req, res, next) => {
+router.use('/:applicationId*', asyncWrap(async(req, res, next) => {
   const application = await req.app.get('db').collection('applications').findOne({
     id: req.params.applicationId
   })
