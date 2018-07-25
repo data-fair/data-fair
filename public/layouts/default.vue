49307a277c70352700467bc705c2ac52e44ffcca
Alban Mouton <alban.mouton@gmail.com>
2018-07-25 14:54:28 +0200

    fix: active buttons when we are in a sub section

--- public/layouts/default.vue
+++ public/layouts/default.vue
@@ -19,10 +19,10 @@
       <!-- larger screens: navigation in toolbar -->
       <v-toolbar-items class="hidden-md-and-down">
         <v-btn flat :to="{name: 'index'}" color="primary" exact>Accueil</v-btn>
-        <v-btn flat to="/datasets" color="primary" :class="routePrefix === 'dataset' ? 'btn--active' : ''">Jeux de données</v-btn>
-        <v-btn flat to="/remote-services" color="primary" :class="routePrefix === 'remote' ? 'btn--active' : ''">Services</v-btn>
-        <v-btn flat to="/applications" color="primary" :class="routePrefix === 'application' ? 'btn--active' : ''">Applications</v-btn>
-        <v-btn flat to="/catalogs" color="primary" :class="routePrefix === 'catalog' ? 'btn--active' : ''">Catalogues</v-btn>
+        <v-btn flat to="/datasets" color="primary" :class="routePrefix === 'dataset' ? 'v-btn--active' : ''">Jeux de données</v-btn>
+        <v-btn flat to="/remote-services" color="primary" :class="routePrefix === 'remote' ? 'v-btn--active' : ''">Services</v-btn>
+        <v-btn flat to="/applications" color="primary" :class="routePrefix === 'application' ? 'v-btn--active' : ''">Applications</v-btn>
+        <v-btn flat to="/catalogs" color="primary" :class="routePrefix === 'catalog' ? 'v-btn--active' : ''">Catalogues</v-btn>
         <template v-if="session.initialized">
           <v-btn v-if="!user" @click="login" color="primary">
             Se connecter / S'inscrire
