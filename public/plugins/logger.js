e4613cb9cda943ed3c300214652762f236ca22da
Alban Mouton <alban.mouton@gmail.com>
2018-09-13 20:39:23 +0200

    chore: debug logs

--- public/plugins/logger.js
+++ public/plugins/logger.js
@@ -3,7 +3,4 @@ import logger from '../logger'
 export default ({env, route}) => {
   const logLevel = (route.query.log || env.browserLogLevel)
   logger.setLevel(logLevel)
-  logger.error('HAHA')
-  logger.warn('warn')
-  logger.debug('debug')
 }
