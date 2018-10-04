d5db58cd86b9f4b06b6ec403aae8c63c22e68ae0
Alban Mouton <alban.mouton@gmail.com>
2018-10-04 14:50:37 +0200

    fix: use full path in analytics

--- public/plugins/analytics.js
+++ public/plugins/analytics.js
@@ -2,5 +2,5 @@ import Vue from 'vue'
 import VueMultianalytics from '@koumoul/vue-multianalytics'
 
 export default ({ store, env, app }) => {
-  Vue.use(VueMultianalytics, { modules: env.analytics, routing: { vueRouter: app.router } })
+  Vue.use(VueMultianalytics, { modules: env.analytics, routing: { vueRouter: app.router, preferredProperty: 'fullPath' } })
 }
