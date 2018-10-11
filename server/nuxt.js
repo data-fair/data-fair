9594adab5bfc657e07d80da391648c06f3594c81
Alban Mouton <alban.mouton@gmail.com>
2018-10-11 17:45:08 +0200

    fix: prevent nuxt getting stuck when re-using the .nuxt directory

--- server/nuxt.js
+++ server/nuxt.js
@@ -1,6 +1,3 @@
-// const path = require('path')
-const fs = require('fs')
-
 const { Nuxt, Builder } = require('nuxt')
 
 const nuxtConfig = require('../nuxt.config.js')
@@ -12,17 +9,11 @@ module.exports = async () => {
   } else {
     // Prepare nuxt for rendering and serving UI
     const nuxt = new Nuxt(nuxtConfig)
-
-    if (fs.existsSync('.nuxt')) {
-      console.log('Nuxt build was already done.')
-    } else {
-      await new Builder(nuxt).build()
-    }
-
-    return (req, res, next) => {
+    await new Builder(nuxt).build()
+    return async (req, res, next) => {
       // re-apply the prefix that was removed by our reverse proxy in prod configs
       req.url = (nuxtConfig.router.base + req.url).replace('//', '/')
-      nuxt.render(req, res, next)
+      nuxt.render(req, res)
     }
   }
 }
