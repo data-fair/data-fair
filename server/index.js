e42bce7ae27ea70f55f19f0ef1a0a02fc874885d
Alban Mouton <alban.mouton@gmail.com>
2018-10-11 14:23:50 +0200

    chore: slightly better log

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = require('./app')
 app.run().then(app => {
   console.log('Web socket and HTTP server listening on http://localhost:%s', config.port)
 }, err => {
-  console.error(err)
+  console.error('Failure while starting server', err)
   process.exit(-1)
 })
 
@@ -15,7 +15,7 @@ process.on('SIGTERM', function onSigterm () {
     console.log('shutting down now')
     process.exit()
   }, err => {
-    console.error(err)
+    console.error('Failure while stopping service', err)
     process.exit(-1)
   })
 })
