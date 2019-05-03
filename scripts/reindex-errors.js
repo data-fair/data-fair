938863cb9ff9e0ab9fddc285feb53e91bba69907
Alban Mouton <alban.mouton@gmail.com>
2019-05-03 21:46:15 +0200

    fix: use dbUtils.connect in scripts

--- scripts/reindex-errors.js
+++ scripts/reindex-errors.js
@@ -1,7 +1,7 @@
 const dbUtils = require('../server/utils/db')
 const datasetUtils = require('../server/utils/dataset')
 async function main() {
-  const { db } = await dbUtils.init()
+  const { db } = await dbUtils.connect()
   const cursor = await db.collection('datasets').find({})
   while (await cursor.hasNext()) {
     const dataset = await cursor.next()
--- scripts/update-consumptions.js
+++ scripts/update-consumptions.js
@@ -1,7 +1,7 @@
 const dbUtils = require('../server/utils/db')
 const datasetUtils = require('../server/utils/dataset')
 async function main() {
-  const { db } = await dbUtils.init()
+  const { db } = await dbUtils.connect()
   const quotas = await db.collection('quotas').find({}).limit(10000).toArray()
   for (let quota of quotas) {
     await datasetUtils.updateStorageSize(db, quota)
