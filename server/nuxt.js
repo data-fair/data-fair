08c3475ed8914773b5d8faa481467f98372cec61
Alban Mouton <alban.mouton@gmail.com>
2019-05-29 17:19:15 +0200

    chore: use nuxt-build-cache to manage .nuxt directory

--- server/nuxt.js
+++ server/nuxt.js
@@ -1,7 +1,7 @@
-const config = require('config')
-const { Nuxt, Builder } = require('nuxt')
+const nuxtBuildCache = require('@koumoul/nuxt-build-cache')
 
 const nuxtConfig = require('../nuxt.config.js')
+const version = process.env.VERSION || require('../package.json').version
 
 module.exports = async () => {
   if (process.env.NODE_ENV === 'development') {
@@ -12,10 +12,7 @@ module.exports = async () => {
     return (req, res, next) => next()
   } else {
     // Prepare nuxt for rendering and serving UI
-    nuxtConfig.dev = config.nuxtDev
-    const nuxt = new Nuxt(nuxtConfig)
-    await nuxt.ready()
-    if (!config.prebuilt) await new Builder(nuxt).build()
+    const nuxt = await nuxtBuildCache.prepare('portals-manager', version, nuxtConfig)
     return async (req, res, next) => {
       // re-apply the prefix that was removed by our reverse proxy in prod configs
       req.url = (nuxtConfig.router.base + req.url).replace('//', '/')
