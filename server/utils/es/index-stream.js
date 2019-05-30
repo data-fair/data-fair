282ad1b27adb179c1515bfb8525118ae72660aa8
Alban Mouton <alban.mouton@gmail.com>
2019-05-30 22:38:24 +0200

    chore: try to make indexing a little more robust

--- server/utils/es/index-stream.js
+++ server/utils/es/index-stream.js
@@ -11,7 +11,7 @@ class IndexStream extends Transform {
     this.i = 0
     this.erroredItems = []
   }
-  _transform(item, encoding, callback) {
+  async _transform(item, encoding, callback) {
     try {
       if (this.options.stats) this.options.stats.count += 1
 
@@ -42,40 +42,51 @@ class IndexStream extends Transform {
 
       if (
         this.body.length / 2 >= config.elasticsearch.maxBulkLines ||
-      this.bulkChars >= config.elasticsearch.maxBulkChars
+        this.bulkChars >= config.elasticsearch.maxBulkChars
       ) {
-        this._sendBulk(callback)
-      } else {
-        callback()
+        await this._sendBulk()
       }
     } catch (err) {
       return callback(err)
     }
+    callback()
   }
-  _final(callback) {
-    this._sendBulk(err => {
-      if (err) return callback(err)
-      this.options.esClient.indices.refresh({ index: this.options.indexName }, callback)
-    })
+  async _final(callback) {
+    try {
+      await this._sendBulk()
+    } catch (err) {
+      return callback(err)
+    }
+
+    try {
+      await this.options.esClient.indices.refresh({ index: this.options.indexName })
+    } catch (err) {
+      // refresh can take some time on large datasets, try one more time
+      await new Promise(resolve => setTimeout(resolve, 30000))
+      try {
+        await this.options.esClient.indices.refresh({ index: this.options.indexName })
+      } catch (err) {
+        console.error('Failure while refreshing index after indexing', err)
+        return callback(new Error('Échec pendant le rafraichissement de la donnée après indexation.'))
+      }
+    }
+    callback()
   }
-  _sendBulk(callback) {
-    if (this.body.length === 0) return callback()
+  async _sendBulk() {
+    if (this.body.length === 0) return
     const bodyClone = [].concat(this.body)
     const sentBulkChars = this.bulkChars
     const bulkOpts = {
       // ES does not want the doc along with a delete instruction,
       // but we put it in body anyway for our outgoing/reporting logic
       body: this.body.filter(line => !line._deleted),
-      timeout: '4m',
-      requestTimeout: 300000
+      timeout: '4m'
     }
-    // Use the ingest plugin to parse attached files
-    if (this.options.attachments) bulkOpts.pipeline = 'attachment'
-    this.options.esClient.bulk(bulkOpts, (err, res) => {
-      if (err) {
-        console.error(`Failure while sending bulk request for indexing: index=${this.options.indexName}, bulkChars=${sentBulkChars}, nbLines=${bodyClone.length / 2}`, err.message)
-        return callback(new Error(`Échec pendant l'indexation d'un paquet de données.`))
-      }
+    try {
+      // Use the ingest plugin to parse attached files
+      if (this.options.attachments) bulkOpts.pipeline = 'attachment'
+      const res = await this.options.esClient.bulk(bulkOpts)
+
       res.items.forEach((item, i) => {
         const _id = (item.index && item.index._id) || (item.update && item.update._id) || (item.delete && item.delete._id)
         const line = this.options.updateMode ? bodyClone[(i * 2) + 1].doc : bodyClone[(i * 2) + 1]
@@ -90,10 +101,12 @@ class IndexStream extends Transform {
           .filter(item => !!item.error)
           .forEach(item => this.erroredItems.push(item))
       }
-      callback()
-    })
-    this.body = []
-    this.bulkChars = 0
+      this.body = []
+      this.bulkChars = 0
+    } catch (err) {
+      console.error(`Failure while sending bulk request for indexing: index=${this.options.indexName}, bulkChars=${sentBulkChars}, nbLines=${bodyClone.length / 2}`, err)
+      throw new Error(`Échec pendant l'indexation d'un paquet de données.`)
+    }
   }
   errorsSummary() {
     if (!this.erroredItems.length) return null
