be54be7ae997931e7c06e42fff58e84ef7db321e
Alban Mouton <alban.mouton@gmail.com>
2020-01-14 09:21:40 +0100

    fix: add BOM to csv in test

--- test/extensions.js
+++ test/extensions.js
@@ -108,7 +108,7 @@ other,unknown address
   // Download extended file
   res = await ax.get('/api/v1/datasets/dataset/full')
   const lines = res.data.split('\n')
-  t.is(lines[0], 'label,adr,lat,Erreur d\'enrichissement')
+  t.is(lines[0], '\ufefflabel,adr,lat,Erreur d\'enrichissement')
   t.is(lines[1], 'koumoul,19 rue de la voie lactée saint avé,40,')
 })
 
