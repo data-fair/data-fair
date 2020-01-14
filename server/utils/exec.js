629abe047bf47955a51dddd29c46d88e2616ee48
Alban Mouton <alban.mouton@gmail.com>
2020-01-14 18:32:34 +0100

    fix: missing await statement

--- server/utils/exec.js
+++ server/utils/exec.js
@@ -2,7 +2,7 @@ const spawn = require('child-process-promise').spawn
 
 module.exports = async (cmd, args) => {
   try {
-    spawn(cmd, args, { capture: ['stderr'] })
+    await spawn(cmd, args, { capture: ['stdout', 'stderr'] })
   } catch (err) {
     throw new Error(err.stderr || err.message)
   }
