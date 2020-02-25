80b04510bc33df98e5007fe4c62a86de9af3c635
Alban Mouton <alban.mouton@gmail.com>
2020-02-25 11:04:59 +0100

    fix: no analytics on embed pages

--- public/plugins/analytics.js
+++ public/plugins/analytics.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueMultianalytics from '@koumoul/vue-multianalytics/src'
 
-export default ({ store, env, app }) => {
-  Vue.use(VueMultianalytics, { modules: env.analytics, routing: { vueRouter: app.router, preferredProperty: 'fullPath' } })
+export default ({ store, env, app, route }) => {
+  if (!route.name.startsWith('embed-')) {
+    Vue.use(VueMultianalytics, { modules: env.analytics, routing: { vueRouter: app.router, preferredProperty: 'fullPath' } })
+  }
 }
