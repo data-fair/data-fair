940793e6941d97d4629fa3388adedb1e8e0334fb
Alban Mouton <alban.mouton@gmail.com>
2020-05-19 15:02:24 +0200

    feat: embedded config page

---
+++ public/pages/embed/application/_id/config.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <application-config />
+</template>
+
+<script>
+  import ApplicationConfig from '~/components/applications/config.vue'
+
+  export default {
+    components: { ApplicationConfig },
+  }
+</script>
+
+<style lang="css">
+</style>
