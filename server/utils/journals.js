696bc7bbc2502086a8ab95ce4607a6d788d7a708
Alban Mouton <alban.mouton@gmail.com>
2020-05-28 16:15:35 +0200

    fix: errors when writing to journal are never blocking for the actual task

--- server/utils/journals.js
+++ server/utils/journals.js
@@ -2,16 +2,21 @@ const moment = require('moment')
 const webhooks = require('./webhooks')
 
 module.exports.log = async function(app, resource, event, type = 'dataset', noStoreEvent) {
-  const db = app.get('db')
-  event.date = moment().toISOString()
+  try {
+    const db = app.get('db')
+    event.date = moment().toISOString()
 
-  if (!noStoreEvent) {
-    await db.collection('journals')
-      .updateOne({ id: resource.id, type }, { $push: { events: event } }, { upsert: true })
+    if (!noStoreEvent) {
+      await db.collection('journals')
+        .updateOne({ id: resource.id, type }, { $push: { events: event } }, { upsert: true })
+    }
+
+    // websockets notifications
+    await app.publish(`${type}s/${resource.id}/journal`, event)
+
+    webhooks.trigger(db, type, resource, event)
+  } catch (err) {
+    // errors when writing to journal are never blocking for the actual task
+    console.warn('Failure when writing event to journal')
   }
-
-  // websockets notifications
-  await app.publish(`${type}s/${resource.id}/journal`, event)
-
-  webhooks.trigger(db, type, resource, event)
 }
