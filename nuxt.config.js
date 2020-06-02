71af5ed41d11e1b4321c265c5d8b57fbe899285d
Alban Mouton <alban.mouton@gmail.com>
2020-06-02 15:45:20 +0200

    fix: cookieDomain missing in sd-vue parameters

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,6 +69,7 @@ module.exports = {
     publicUrl: config.publicUrl,
     wsPublicUrl: config.wsPublicUrl,
     directoryUrl: config.directoryUrl,
+    sessionDomain: config.sessionDomain,
     adminRole: config.adminRole,
     map: config.map,
     brand: config.brand,
--- public/plugins/session.js
+++ public/plugins/session.js
@@ -1,5 +1,9 @@
 export default ({ store, app, env }) => {
   store.commit('setAny', { env: { ...env } })
-  store.dispatch('session/init', { cookies: app.$cookies, baseUrl: env.publicUrl + '/api/v1/session' })
+  store.dispatch('session/init', {
+    cookies: app.$cookies,
+    baseUrl: env.publicUrl + '/api/v1/session',
+    cookieDomain: env.sessionDomain,
+  })
   store.dispatch('session/loop', app.$cookies)
 }
