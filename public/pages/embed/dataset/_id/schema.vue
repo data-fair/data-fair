8f3d3564fadb38052db55714e244a41ad71f5ac1
Alban Mouton <alban.mouton@gmail.com>
2020-06-24 17:27:04 +0200

    feat: add schema embed page

---
+++ public/pages/embed/dataset/_id/schema.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+  <dataset-schema />
+</template>
+
+<script>
+  import 'iframe-resizer/js/iframeResizer.contentWindow'
+  import DatasetSchema from '~/components/datasets/schema.vue'
+
+  export default {
+    components: { DatasetSchema },
+  }
+</script>
