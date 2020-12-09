7c95fc6ae87ead09c8651aeb5a41fcb492eb24f0
Alban Mouton <alban.mouton@gmail.com>
2020-12-09 21:52:55 +0100

    fix: children are required for virtual dataset

--- public/components/datasets/create-virtual.vue
+++ public/components/datasets/create-virtual.vue
@@ -22,6 +22,8 @@
         :items="datasets"
         :loading="loadingDatasets"
         :search-input.sync="search"
+        :required="true"
+        :rules="[value => (value && value.length) || '']"
         hide-no-data
         item-text="title"
         item-value="id"
