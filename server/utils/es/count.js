9c2429fb54621d5f8ec5bcc1da624258131d5122
Alban Mouton <alban.mouton@gmail.com>
2020-12-16 17:34:22 +0100

    fix: broken ES count

--- server/utils/es/count.js
+++ server/utils/es/count.js
@@ -3,5 +3,5 @@ const { aliasName, prepareQuery } = require('./commons')
 module.exports = async (client, dataset, query) => {
   const esQuery = prepareQuery(dataset, query)
   const esResponse = await client.count({ index: aliasName(dataset), body: { query: esQuery.query } })
-  return esResponse.count
+  return esResponse.body.count
 }
