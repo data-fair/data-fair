b2884efdab62e3f5548f1cd6bebe4c15e4aa5d42
Alban Mouton <alban.mouton@gmail.com>
2020-12-17 09:44:08 +0100

    fix: change finalizedAt in upgrade script to invalidate some caches

---
+++ upgrade/scripts/2.16.0/invalidate-tiles-cache.js
@@ -0,0 +1,20 @@
+const fs = require('fs-extra')
+const datasetUtils = require('../../../server/utils/dataset')
+
+  exports.description = 'Update finalizedAt metadata of some datasets so that tile caches are invalidated'
+
+  exports.exec = async (db, debug) => {
+    const cursor = db.collection('datasets').find({
+      bbox: { $exists: true },
+    })
+    const finalizedAt = new Date().toISOString()
+    for await (const dataset of cursor) {
+      if (dataset.originalFile) {
+        const mbtilesPath = datasetUtils.extFileName(dataset, 'mbtiles')
+        if (await fs.exists(mbtilesPath)) continue
+      }
+
+      debug(`change finalizedAt (${dataset.finalizedAt} -> ${finalizedAt}) for dataset ${dataset.id}`)
+      await db.collection('datasets').updateOne({ _id: dataset._id }, { $set: { finalizedAt } })
+    }
+  }
