d494f04378dd4947a68c15343eecb07356f06ff9
Alban Mouton <alban.mouton@gmail.com>
2021-01-08 09:45:27 +0100

    fix: typo

--- server/utils/es/index.js
+++ server/utils/es/index.js
@@ -20,7 +20,7 @@ exports.errorMessage = (errBody) => {
   let message
   if (errBody.reason) message = errBody.reason
   if (errBody.root_cause && errBody.root_cause.reason) message = errBody.root_cause.reason
-  if (errBody.root_cause && errBody.root_cause[0].reason && errBody.root_cause[0].reason) message = errBody.root_cause[0].reason
+  if (errBody.root_cause && errBody.root_cause[0] && errBody.root_cause[0].reason) message = errBody.root_cause[0].reason
   if (errBody.failed_shards && errBody.failed_shards[0] && errBody.failed_shards[0].reason) {
     const shardReason = errBody.failed_shards[0].reason
     if (shardReason.caused_by && shardReason.caused_by.reason) {
