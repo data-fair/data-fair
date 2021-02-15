1401f8a2112dc4d24c674e46c09c4b19dd8890c2
Alban Mouton <alban.mouton@gmail.com>
2021-02-15 21:10:51 +0100

    fix: use hostname from public url in api server description

--- contract/dataset-api-docs.js
+++ contract/dataset-api-docs.js
@@ -290,15 +290,14 @@ Pour utiliser cette API dans un programme vous aurez besoin d'une clé que vous 
     `
   }
 
-  const hostname = new URL(config.masterDataPublicUrl).hostname
   const servers = [{
     url: `${config.publicUrl}/api/v1/datasets/${dataset.id}`,
-    description: `Instance DataFair - ${hostname}`,
+    description: `Instance DataFair - ${new URL(config.publicUrl).hostname}`,
   }]
   if (dataset.masterData && dataset.masterData.length && config.masterDataPublicUrl && config.masterDataPublicUrl !== config.publicUrl) {
     servers.push({
       url: `${config.masterDataPublicUrl}/api/v1/datasets/${dataset.id}`,
-      description: `Accès DataFair MasterData - ${hostname}`,
+      description: `Accès DataFair MasterData - ${new URL(config.masterDataPublicUrl).hostname}`,
     })
   }
 
