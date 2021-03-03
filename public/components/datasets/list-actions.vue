07b4262bc4611eaf672ddc5afc1bd55f5a611540
Alban Mouton <alban.mouton@gmail.com>
2021-03-03 11:37:29 +0100

    chore: add tooltip on datasets actions

--- public/components/datasets/list-actions.vue
+++ public/components/datasets/list-actions.vue
@@ -3,30 +3,57 @@
     <v-subheader>
       CRÉER UN JEU DE DONNÉES
     </v-subheader>
-    <v-list-item :to="{path: '/new-dataset', query: {type: 'file'}}">
-      <v-list-item-icon>
-        <v-icon color="primary">
-          mdi-file-upload
-        </v-icon>
-      </v-list-item-icon>
-      <v-list-item-title>Fichier</v-list-item-title>
-    </v-list-item>
-    <v-list-item :to="{path: '/new-dataset', query: {type: 'virtual'}}">
-      <v-list-item-icon>
-        <v-icon color="primary">
-          mdi-picture-in-picture-bottom-right-outline
-        </v-icon>
-      </v-list-item-icon>
-      <v-list-item-title>Virtuel</v-list-item-title>
-    </v-list-item>
-    <v-list-item :to="{path: '/new-dataset', query: {type: 'rest'}}">
-      <v-list-item-icon>
-        <v-icon color="primary">
-          mdi-all-inclusive
-        </v-icon>
-      </v-list-item-icon>
-      <v-list-item-title>Incrémental</v-list-item-title>
-    </v-list-item>
+    <v-tooltip left>
+      <template v-slot:activator="{ on, attrs }">
+        <v-list-item
+          :to="{path: '/new-dataset', query: {type: 'file'}}"
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-list-item-icon>
+            <v-icon color="primary">
+              mdi-file-upload
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Fichier</v-list-item-title>
+        </v-list-item>
+      </template>
+      <span>Chargez un fichier parmi les nombreux formats supportés.</span>
+    </v-tooltip>
+    <v-tooltip left>
+      <template v-slot:activator="{ on, attrs }">
+        <v-list-item
+          :to="{path: '/new-dataset', query: {type: 'rest'}}"
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-list-item-icon>
+            <v-icon color="primary">
+              mdi-all-inclusive
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Incrémental</v-list-item-title>
+        </v-list-item>
+      </template>
+      <span>Créez un jeu de données dont le contenu sera saisissable directement avec un formulaire en ligne.</span>
+    </v-tooltip>
+    <v-tooltip left>
+      <template v-slot:activator="{ on, attrs }">
+        <v-list-item
+          :to="{path: '/new-dataset', query: {type: 'virtual'}}"
+          v-bind="attrs"
+          v-on="on"
+        >
+          <v-list-item-icon>
+            <v-icon color="primary">
+              mdi-picture-in-picture-bottom-right-outline
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Virtuel</v-list-item-title>
+        </v-list-item>
+      </template>
+      <span>Créez une vue virtuelle réduite sur un ou plusieurs jeux de données.</span>
+    </v-tooltip>
   </v-list>
 </template>
 
