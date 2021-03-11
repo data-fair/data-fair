02dfff63e25f86eed4e850b28e00f71022bf537d
Alban Mouton <alban.mouton@gmail.com>
2021-03-11 10:31:40 +0100

    fix: no treemap if there are no datasets

--- public/components/storage/treemap.vue
+++ public/components/storage/treemap.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :style="`position:relative;width:100%;height:${height}px;`">
+  <div v-if="datasets.count" :style="`position:relative;width:100%;height:${height}px;`">
     <div
       v-for="box in boxes || []"
       :key="box.data.id"
