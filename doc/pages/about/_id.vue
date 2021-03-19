228d5b2351cb3f07d1690bec30924f51235da4a7
Alban Mouton <alban.mouton@gmail.com>
2021-03-19 15:12:30 +0100

    fix: missing file in doc project

---
+++ doc/pages/about/_id.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <doc-page :content="content" />
+</template>
+
+<script>
+  import DocPage from '~/components/DocPage'
+  // Webpack way of requiring a bunch of modules at once
+  const context = require.context('./', true, /\.md$/)
+
+  export default {
+    components: { DocPage },
+    computed: {
+      content() {
+        if (!this.$route) return
+        const content = context(`./${this.$route.params.id}-fr.md`)
+        return content.default
+      },
+    },
+    head() {
+      const meta = [{ hid: 'robots', name: 'robots', content: 'index' }]
+      return { meta }
+    },
+  }
+</script>
