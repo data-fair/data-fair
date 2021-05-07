5b65ee8da3f3c56ee823e63a6a6c8ea019d6d0d2
Alban Mouton <alban.mouton@gmail.com>
2021-05-07 10:47:56 +0200

    fix: safer test

--- test/truncate.js
+++ test/truncate.js
@@ -5,17 +5,19 @@ const workers = require('../server/workers')
 describe('truncate', () => {
   it('Truncate results for faster previews', async () => {
     const ax = global.ax.dmeadus
-    let res = await ax.post('/api/v1/datasets', {
+    await ax.post('/api/v1/datasets', {
       isRest: true,
       title: 'truncate1',
       schema: [{ key: 'str', type: 'string' }],
     })
-    res = await ax.post('/api/v1/datasets/truncate1/lines', { str: 'bla' })
-    res = await ax.post('/api/v1/datasets/truncate1/lines', { str: 'blablabla' })
+    await ax.post('/api/v1/datasets/truncate1/_bulk_lines', [
+      { str: 'bla' },
+      { str: 'blablabla' },
+    ])
     await workers.hook('indexer/truncate1')
-    res = await ax.get('/api/v1/datasets/truncate1/lines', { params: { truncate: '4' } })
+    const res = await ax.get('/api/v1/datasets/truncate1/lines', { params: { truncate: '4', sort: '_i' } })
     assert.equal(res.data.results.length, 2)
-    assert.equal(res.data.results[1].str, 'bla')
-    assert.equal(res.data.results[0].str, 'blab...')
+    assert.equal(res.data.results[0].str, 'bla')
+    assert.equal(res.data.results[1].str, 'blab...')
   })
 })
