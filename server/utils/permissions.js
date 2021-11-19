abbe772f9ca0e7719aff3b20af499469e4d8eb94
Alban Mouton <alban.mouton@gmail.com>
2021-11-19 08:12:10 +0100

    fix: encode header content

--- server/utils/permissions.js
+++ server/utils/permissions.js
@@ -9,7 +9,7 @@ const validate = ajv.compile(permissionsSchema)
 exports.middleware = function(operationId, operationClass, trackingCategory) {
   return function(req, res, next) {
     // these headers can be used to apply other permission/quota/metrics on the gateway
-    if (req.resource) res.setHeader('x-resource', JSON.stringify({ type: req.resourceType, id: req.resource.id, title: req.resource.title }))
+    if (req.resource) res.setHeader('x-resource', JSON.stringify({ type: req.resourceType, id: req.resource.id, title: encodeURIComponent(req.resource.title) }))
     if (req.resource && req.resource.owner) res.setHeader('x-owner', JSON.stringify({ type: req.resource.owner.type, id: req.resource.owner.id }))
     req.operation = { class: operationClass, id: operationId, track: trackingCategory }
     res.setHeader('x-operation', JSON.stringify(req.operation))
