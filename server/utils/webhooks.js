53e18586cda8b533774f4f7305c2cd19572a273c
Alban Mouton <alban.mouton@gmail.com>
2021-11-29 15:05:11 +0100

    fix: use event type from webhooks schema when sending webhook

--- server/utils/webhooks.js
+++ server/utils/webhooks.js
@@ -2,13 +2,14 @@ const axios = require('axios')
 const config = require('config')
 const { notify } = require('superagent')
 const ua = require('universal-analytics')
-const events = require('../../shared/events.json')
+const { items } = require('../../contract/publication-sites')
+const settingsSchema = require('../../contract/settings')
 const notifications = require('./notifications')
 const debug = require('debug')('webhooks')
 
 exports.trigger = async (db, type, resource, event) => {
-  const eventType = events[type][event.type]
-  if (!eventType) return console.error('Unknown event type', type, event.type)
+  const eventType = settingsSchema.properties.webhooks.items.properties.events.items.oneOf.find(eventType => items.const === eventType)
+  if (!eventType) return debug('Unknown webhook event type', type, event.type)
   // first send notifications before actual webhooks
   const sender = { ...resource.owner }
   delete sender.role
@@ -16,7 +17,7 @@ exports.trigger = async (db, type, resource, event) => {
   const notif = {
     sender,
     topic: { key: `${prefix}${event.type}:${resource.id}` },
-    title: eventType.text,
+    title: eventType.title,
     // body: event.data || '',
     body: resource.title || resource.id,
     urlParams: { id: resource.id },
@@ -35,7 +36,7 @@ exports.trigger = async (db, type, resource, event) => {
     try {
       // Simple HTTP POST (mattermost, etc.)
       if (webhook.target.type === 'http') {
-        let text = (resource.title || resource.id) + ' - ' + eventType.text + (event.href ? ' - ' + event.href : '')
+        let text = (resource.title || resource.id) + ' - ' + eventType.title + (event.href ? ' - ' + event.href : '')
         if (event.data) text += '\n\n' + event.data
         await axios.post(webhook.target.params.url, {
           text,
@@ -54,7 +55,7 @@ exports.trigger = async (db, type, resource, event) => {
         await new Promise((resolve, reject) => {
           let label = resource.title
           if (event.data) label += '\n\n' + event.data
-          visitor.event(type, eventType.text, label, err => {
+          visitor.event(type, eventType.title, label, err => {
             if (err) reject(err)
             else resolve()
           })
