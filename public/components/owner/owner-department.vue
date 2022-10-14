2d51781ba2d91c3d0295a6ab46933b70758c800d
Alban Mouton <alban.mouton@gmail.com>
2022-10-14 17:15:44 +0200

    feat: use department avatar

--- public/components/owner/owner-department.vue
+++ public/components/owner/owner-department.vue
@@ -1,19 +1,37 @@
 <template>
-  <span
+  <v-tooltip
     v-if="owner && owner.department"
-    class="text-caption"
-    style="max-width: 100px;"
+    top
   >
-    {{ owner.department }}
-  </span>
+    <template #activator="{on}">
+      <span
+        class="text-body-2"
+        v-on="on"
+      >
+        <v-avatar :size="28">
+          <img :src="`${env.directoryUrl}/api/avatars/${owner.type}/${owner.id}/${owner.department}/avatar.png`">
+        </v-avatar>
+      </span>
+    </template>
+    {{ label }}
+  </v-tooltip>
+  <!--<span v-if="dataset.owner.type === 'user'"><v-icon>mdi-account</v-icon>&nbsp;{{ dataset.owner.name }}</span>
+  <span v-if="dataset.owner.type === 'organization'"><v-icon>mdi-account-group</v-icon>&nbsp;{{ dataset.owner.name }}<span v-if="dataset.owner.role"> ({{ dataset.owner.role }})</span></span>-->
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
-  props: ['owner']
+  props: ['owner'],
+  computed: {
+    ...mapState(['env']),
+    label () {
+      return this.owner.departmentName || this.owner.department
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="css" scoped>
 </style>
