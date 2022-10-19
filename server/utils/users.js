8f076c70d37a410dc9f5cb4b9ffdf8d199292b29
Alban Mouton <alban.mouton@gmail.com>
2022-10-19 11:46:25 +0200

    feat: add departmentName to owner info

--- server/utils/users.js
+++ server/utils/users.js
@@ -5,8 +5,10 @@ exports.owner = (req) => {
   // for other people it is based on the active account
   const orga = req.user.organization
   if (orga) {
-    if (orga.department) return { type: 'organization', id: orga.id, name: orga.name, department: orga.department }
-    else return { type: 'organization', id: orga.id, name: orga.name }
+    const owner = { type: 'organization', id: orga.id, name: orga.name }
+    if (orga.department) owner.department = orga.department
+    if (orga.departmentName) owner.departmentName = orga.departmentName
+    return owner
   }
   return { type: 'user', id: req.user.id, name: req.user.name }
 }
