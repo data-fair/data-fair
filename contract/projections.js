cc2ac52044c61d76a99562b41d6492fcc7960932
Alban Mouton <alban.mouton@gmail.com>
2022-12-08 18:02:40 +0100

    feat: add some more projections

--- contract/projections.js
+++ contract/projections.js
@@ -10,11 +10,19 @@ module.exports = [{
   code: 'EPSG:2154',
   proj4: '+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs '
 }, {
+  title: 'RGAF09 / UTM zone 20N',
+  code: 'EPSG:5490',
+  proj4: '+proj=utm +zone=20 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs'
+}, {
   title: 'WGS 84 / Pseudo-Mercator -- Spherical Mercator, Google Maps, OpenStreetMap, Bing, ArcGIS, ESRI',
   code: 'EPSG:3857',
   proj4: '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs'
 }, {
-  title: 'RGAF09 / UTM zone 20N',
-  code: 'EPSG:5490',
-  proj4: '+proj=utm +zone=20 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs'
+  title: 'WGS 84 - WGS84 - World Geodetic System 1984, used in GPS',
+  code: 'EPSG:4326',
+  proj4: '+proj=longlat +datum=WGS84 +no_defs +type=crs'
+}, {
+  title: 'WGS 84 / UTM zone 20N',
+  code: 'EPSG:32620',
+  proj4: '+proj=utm +zone=20 +datum=WGS84 +units=m +no_defs +type=crs'
 }]
