90281b85c44b1c0f01d604c97b38dcbbbd2baa66
Alban Mouton <alban.mouton@gmail.com>
2023-01-02 12:03:52 +0100

    feat: add headers to track embed pages in metrics

--- server/nuxt.js
+++ server/nuxt.js
@@ -1,9 +1,17 @@
 const config = require('config')
 
 module.exports = async () => {
+  const trackEmbed = (req, res, next) => {
+    if (!req.url.startsWith('/embed/')) return next()
+    const [resourceType, resourceId, embedView] = req.url.replace('/embed/', '').split(/[/?]/)
+    res.setHeader('x-resource', JSON.stringify({ type: 'embed', id: `${resourceType}-${resourceId}-${embedView}`, title: encodeURIComponent(`${resourceId} / ${embedView}`) }))
+    res.setHeader('x-operation', JSON.stringify({ class: 'read', id: 'openEmbed', track: 'openApplication' }))
+    next()
+  }
   if (config.proxyNuxt) {
     // in dev mode the nuxt dev server is already running, we re-expose it
     return {
+      trackEmbed,
       render: require('http-proxy-middleware').createProxyMiddleware({
         target: 'http://localhost:3000/data-fair/'
         // pathRewrite: { '^/data-fair': '' }
@@ -11,7 +19,7 @@ module.exports = async () => {
     }
   } else if (process.env.NODE_ENV === 'test') {
     // no UI during tests
-    return { render: (req, res, next) => next() }
+    return { trackEmbed, render: (req, res, next) => next() }
   } else {
     const { Nuxt } = require('nuxt-start')
     const nuxtConfig = require('../nuxt.config.js')
@@ -20,6 +28,7 @@ module.exports = async () => {
     nuxtConfig.dev = false
     const nuxt = new Nuxt(nuxtConfig)
     return {
+      trackEmbed,
       render: async (req, res, next) => {
         // force buffering (necessary for caching) of this response in the reverse proxy
         res.setHeader('X-Accel-Buffering', 'yes')
