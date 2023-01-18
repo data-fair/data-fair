09ade77a060dd1ffedd65a9eb10ae2853bd27054
Alban Mouton <alban.mouton@gmail.com>
2023-01-18 15:01:16 +0100

    fix: missing delete operation in permission upgrade script

--- upgrade/scripts/3.51.4/explicit-permissions.js
+++ upgrade/scripts/3.51.4/explicit-permissions.js
@@ -1,14 +1,15 @@
 exports.description = 'contribs do not have an implicit write permission anymore'
 
 exports.exec = async (db, debug) => {
-  const initPermissions = (resource, classes) => {
+  const initPermissions = (resource, classes, operations = []) => {
     const contribPermission = {
       type: resource.owner.type,
       id: resource.owner.id,
       name: resource.owner.name,
       department: resource.owner.department || '-',
       roles: ['contrib'],
-      classes
+      classes,
+      operations
     }
     if (resource.owner.departmentName) {
       contribPermission.departmentName = resource.owner.departmentName
@@ -23,7 +24,7 @@ exports.exec = async (db, debug) => {
       permissions: { $not: { $elemMatch: { roles: 'contrib', classes: 'write' } } }
     })) {
       debug(`resource ${resourceType} / ${resource.id}`)
-      const permissions = initPermissions(resource, ['write'])
+      const permissions = initPermissions(resource, ['write'], ['delete'])
       await db.collection(resourceType).updateOne(
         { id: resource.id },
         { $set: { permissions } }
