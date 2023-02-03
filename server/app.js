e58812ffc3b11cdb8c5f3e2b57ebefa518e4f66c
Alban Mouton <alban.mouton@gmail.com>
2023-02-03 15:46:29 +0100

    chore: longer incoming socket timeout

--- server/app.js
+++ server/app.js
@@ -142,6 +142,11 @@ if (config.mode.includes('server')) {
 
   const WebSocket = require('ws')
   server = require('http').createServer(app)
+  // cf https://connectreport.com/blog/tuning-http-keep-alive-in-node-js/
+  // timeout is often 60s on the reverse proxy, better to a have a longer one here
+  // so that interruption is managed downstream instead of here
+  server.keepAliveTimeout = (60 * 1000) + 1000
+  server.headersTimeout = (60 * 1000) + 2000
   wss = new WebSocket.Server({ server })
 }
 
