f873c22b278c6c8c04d0d4101bfb2a0434ac7842
Alban Mouton <alban.mouton@gmail.com>
2023-02-06 15:22:21 +0100

    Revert "fix: better encoding of thumbor image path"
    This reverts commit 8cb780eb247446787430328c3879f3f4afd68a6e.

--- server/utils/thumbor.js
+++ server/utils/thumbor.js
@@ -5,19 +5,13 @@ const thumbor = new Thumbor(config.thumbor.key, config.thumbor.url)
 
 exports.thumbnail = (url, size, thumbnailsOpts = {}) => {
   if (!url) return null
+  url = encodeURI(decodeURI(url))
   if (size === 'true' || size === '1') size = '300x200'
-  size = size.split('x')
-
-  // url can be given encoded or not, decoding should not hurt in any case
-  url = decodeURI(url)
   // special case as thumbor does not create thumbnails of SVG images by default
   if (url.toLowerCase().endsWith('.svg')) return url
-  // thumbor prefers resolving the scheme
-  url = url.replace('http://', '').replace('https://', '')
-  // url is encoded as a single URI param for thumbor
-  url = encodeURIComponent(url)
+  size = size.split('x')
 
-  const instance = thumbor.setImagePath(url)
+  const instance = thumbor.setImagePath(url.replace('http://', '').replace('https://', ''))
   if (thumbnailsOpts.resizeMode === 'fitIn') instance.fitIn(size[0], size[1])
   else instance.resize(size[0], size[1])
   if (thumbnailsOpts.resizeMode === 'smartCrop') instance.smartCrop(true)
