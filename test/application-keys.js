111b706ba9b309e21632d54f7ef7681abbd41c3b
Alban Mouton <alban.mouton@gmail.com>
2023-02-07 18:37:01 +0100

    tests: safer test

--- test/application-keys.js
+++ test/application-keys.js
@@ -162,6 +162,7 @@ describe('Applications keys for unauthenticated readOnly access', () => {
       title: 'restcrowdown',
       schema: [{ key: 'attr1', type: 'string' }, { key: 'attr2', type: 'string' }]
     })
+    await workers.hook('finalizer/restcrowdown')
 
     res = await ax.put('/api/v1/applications/' + appId + '/config', {
       datasets: [{
