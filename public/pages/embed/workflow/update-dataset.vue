6154263189626330f90381e060c0f249920a4b11
Alban Mouton <alban.mouton@gmail.com>
2023-02-23 15:48:16 +0100

    fix: proper iframe resize in update-dataset workflow

---
+++ public/pages/embed/workflow/update-dataset.vue
@@ -0,0 +1,13 @@
+<template>
+  <workflow-update-dataset
+    data-iframe-height
+    :dataset-params="{publicationSites: $route.query.publicationSite, owner: $route.query.owner}"
+  />
+</template>
+
+<script>
+export default {
+  layout: 'embed',
+  middleware: ['auth-required']
+}
+</script>
