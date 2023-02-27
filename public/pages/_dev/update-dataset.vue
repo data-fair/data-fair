699a3cdacccddfd06e77aa7e8dec5363ace90f2a
Alban Mouton <alban.mouton@gmail.com>
2023-02-27 15:29:36 +0100

    fix: bug in v-iframe state synchronization

---
+++ public/pages/_dev/update-dataset.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <div>
+    <v-btn to="/_dev/update-dataset">back</v-btn>
+    <v-iframe
+      src="http://localhost:5600/data-fair/embed/workflow/update-dataset"
+      :query-params-extra="{
+        primary: '#FF00FF'
+      }"
+      :sync-state="true"
+    />
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import 'iframe-resizer/js/iframeResizer'
+import VIframe from '@koumoul/v-iframe'
+
+export default {
+  components: {
+    VIframe
+  },
+  computed: {
+    ...mapState(['env'])
+  }
+}
+</script>
+
+<style lang="css">
+.open-api .v-iframe iframe {
+  background-color: white;
+  border: none;
+}
+</style>
