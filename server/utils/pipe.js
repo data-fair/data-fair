d41d96a245ec49e97c137624c104e6db939426e5
Alban Mouton <alban.mouton@gmail.com>
2023-03-20 09:24:47 +0100

    chore: remove verbose log

--- server/utils/pipe.js
+++ server/utils/pipe.js
@@ -1,10 +1,11 @@
 // a small tool to switch between pump and pipeline to check if one of them is better
 // at handling some occasional "premature close" errors that crash the service
+const debug = require('debug')('pipe')
 
 if (process.env.PIPE === 'pipeline') {
-  console.log('PIPE: use native pipeline method to pipe streams together')
+  debug('PIPE: use native pipeline method to pipe streams together')
   module.exports = require('stream/promises').pipeline
 } else {
-  console.log('PIPE: use npm pump module to pipe streams together')
+  debug('PIPE: use npm pump module to pipe streams together')
   module.exports = require('util').promisify(require('pump'))
 }
