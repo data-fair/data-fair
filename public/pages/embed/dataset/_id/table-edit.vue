3f75178ccdfeb0580a076e562c36a808b4d3301b
Alban Mouton <alban.mouton@gmail.com>
2023-03-28 11:12:02 +0200

    feat: make table-edit iframe use iframe-resizer

--- public/pages/embed/dataset/_id/table-edit.vue
+++ public/pages/embed/dataset/_id/table-edit.vue
@@ -2,12 +2,19 @@
   <v-container
     fluid
     class="pa-0 embed-table-edit"
+    data-iframe-height
   >
     <dataset-table />
   </v-container>
 </template>
 
 <script>
+import 'iframe-resizer/js/iframeResizer.contentWindow'
+
+global.iFrameResizer = {
+  heightCalculationMethod: 'taggedElement'
+}
+
 export default {
   mounted () {
     this.$store.dispatch('dataset/subscribe')
