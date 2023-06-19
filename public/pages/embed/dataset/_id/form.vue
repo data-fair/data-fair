e609918641a472ebc0c6886539ba3020fcc0216e
Alban Mouton <alban.mouton@gmail.com>
2023-06-19 17:30:23 +0200

    feat: embedded form can create owner only lines

--- public/pages/embed/dataset/_id/form.vue
+++ public/pages/embed/dataset/_id/form.vue
@@ -1,24 +1,25 @@
 <template lang="html">
   <v-container data-iframe-height>
     <v-alert
-      v-if="!dataset.isRest"
+      v-if="dataset && !dataset.isRest"
       type="error"
     >
       Ces données ne sont pas éditables
     </v-alert>
     <v-alert
-      v-else-if="!dataset.userPermissions.includes('createLine')"
+      v-else-if="dataset && !dataset.userPermissions.includes('createLine')"
       type="error"
     >
       Vous n'avez pas la permission de saisir ces données
     </v-alert>
-    <template v-else>
+    <template v-else-if="dataset || jsonSchema">
       <v-form
         ref="editLineForm"
         v-model="valid"
       >
         <dataset-edit-line-form
           v-model="line"
+          :own-lines="ownLinesMode"
           @onFileUpload="onFileUpload"
         />
         <v-row>
@@ -40,7 +41,7 @@
 
 <script>
 import 'iframe-resizer/js/iframeResizer.contentWindow'
-import { mapState } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 
 global.iFrameResizer = {
   heightCalculationMethod: 'taggedElement'
@@ -55,7 +56,12 @@ export default {
     file: null
   }),
   computed: {
-    ...mapState('dataset', ['dataset'])
+    ...mapGetters('session', ['activeAccount']),
+    ...mapState('dataset', ['dataset', 'jsonSchema']),
+    ...mapGetters('dataset', ['resourceUrl']),
+    ownLinesMode () {
+      return (this.dataset && this.dataset.rest && this.dataset.rest.lineOwnership) || (!this.dataset && !!this.jsonSchema)
+    }
   },
   methods: {
     onFileUpload (file) {
@@ -65,7 +71,11 @@ export default {
       if (!this.$refs.editLineForm.validate()) return
       this.saving = true
       await new Promise(resolve => setTimeout(resolve, 100))
-      await this.$store.dispatch('dataset/saveLine', { line: this.line, file: this.file })
+      if (this.ownLinesMode) {
+        await this.$axios.$post(`${this.resourceUrl}/own/${this.activeAccount.type}:${this.activeAccount.id}/lines`, this.line)
+      } else {
+        await this.$store.dispatch('dataset/saveLine', { line: this.line, file: this.file })
+      }
       this.saving = false
       this.sent = true
     }
