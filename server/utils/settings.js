831882ba154f074e4b108578c509ba76e8848bc1
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2023-09-05 14:44:59 +0200

    fix: concepts id retrieved for private vocabulary

--- server/utils/settings.js
+++ server/utils/settings.js
@@ -3,7 +3,13 @@ const i18nUtils = require('./i18n')
 exports.getPrivateOwnerVocabulary = async (db, owner) => {
   const settings = await db.collection('settings')
     .findOne({ type: owner.type, id: owner.id }, { projection: { privateVocabulary: 1 } })
-  return (settings && settings.privateVocabulary) || []
+  return (settings && settings.privateVocabulary).map(pv => {
+    const id = `${owner.type.slice(0, 1)}_${owner.id}_${pv.id}`
+    return {
+      ...pv,
+      id
+    }
+  }) || []
 }
 
 exports.getFullOwnerVocabulary = async (db, owner, locale) => {
@@ -12,14 +18,12 @@ exports.getFullOwnerVocabulary = async (db, owner, locale) => {
   const privateVocabulary = await exports.getPrivateOwnerVocabulary(db, owner)
 
   return i18nUtils.vocabularyArray[locale].concat(privateVocabulary.map(pv => {
-    const id = `${owner.type.slice(0, 1)}_${owner.id}_${pv.id}`
     // apply a owner prefix to make the concept id unique
     pv.identifiers = pv.identifiers.filter(i => !!i)
     // we do this to maintain compatibility for pieces of code that expect identifiers to be defined
-    const identifiers = pv.identifiers.length ? pv.identifiers : [id]
+    const identifiers = pv.identifiers.length ? pv.identifiers : [pv.id]
     return {
       ...pv,
-      id,
       identifiers,
       private: true
     }
