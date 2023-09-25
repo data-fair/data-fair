348bb2feb6ac674272b408ff8370a0ea8fcfdc84
Alban Mouton <alban.mouton@gmail.com>
2023-09-25 15:05:20 +0200

    fix: typo in upgrade script

--- upgrade/scripts/3.72.2/publication-ids.js
+++ upgrade/scripts/3.72.2/publication-ids.js
@@ -7,6 +7,6 @@ exports.exec = async (db, debug) => {
   }
   for await (const application of db.collection('applications').find({ slug: { $exists: false } }).project({ id: 1 })) {
     debug(`work on application ${application.id}`)
-    await db.collection('datasets').updateOne({ id: application.id }, { $set: { slug: application.id } })
+    await db.collection('applications').updateOne({ id: application.id }, { $set: { slug: application.id } })
   }
 }
