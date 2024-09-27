81e50e8b13d49aebca0f1db752fb49af0c6e07f3
Alban Mouton <alban.mouton@gmail.com>
2024-09-27 09:32:42 +0200

    fix: remove incorrect null values in remoteFile info

--- upgrade/scripts/4.20.0/null-remotefile-props.js
+++ upgrade/scripts/4.20.0/null-remotefile-props.js
@@ -2,13 +2,13 @@ exports.description = 'Remove null values in remoteFile.etag and remoteFile.last
 
 exports.exec = async (db, debug) => {
   const lastModifiedRes = await db.collection('datasets').updateMany(
-    { 'remoteFile.lastModified': { $exists: true, $eq: true } },
+    { 'remoteFile.lastModified': { $exists: true, $eq: null } },
     { $unset: { 'remoteFile.lastModified': 1 } }
   )
   debug('removed null remoteFile.lastModified', lastModifiedRes)
 
   const etagRes = await db.collection('datasets').updateMany(
-    { 'remoteFile.etag': { $exists: true, $eq: true } },
+    { 'remoteFile.etag': { $exists: true, $eq: null } },
     { $unset: { 'remoteFile.etag': 1 } }
   )
   debug('removed null remoteFile.etag', etagRes)
