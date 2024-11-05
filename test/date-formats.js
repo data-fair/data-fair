4850d19bed5e45c563d7e81f0287507d1dccf227
Alban Mouton <alban.mouton@gmail.com>
2024-11-05 11:24:49 +0100

    feat: more permissive loading of csv on editable dataset

--- test/date-formats.js
+++ test/date-formats.js
@@ -12,14 +12,14 @@ describe('Date formats', () => {
 
     assert.equal(res.data.total, 3)
     assert.equal(res.data.results[0].date, '2021-01-13')
-    assert.equal(res.data.results[0].datefr, '2021-01-13')
+    assert.equal(res.data.results[0].date_fr, '2021-01-13')
     assert.equal(res.data.results[0].datetime, '2021-01-13T19:42:02.790Z')
-    assert.ok(res.data.results[0].datetimefr.startsWith('2021-01-13T'))
+    assert.ok(res.data.results[0].datetime_fr.startsWith('2021-01-13T'))
 
     assert.equal(res.data.results[2].date, '2021-01-15')
-    assert.equal(res.data.results[2].datefr, '2021-01-15')
+    assert.equal(res.data.results[2].date_fr, '2021-01-15')
     assert.equal(res.data.results[2].datetime, '2021-01-15T19:42:02.790Z')
-    assert.ok(res.data.results[2].datetimefr.startsWith('2021-01-15T'))
+    assert.ok(res.data.results[2].datetime_fr.startsWith('2021-01-15T'))
   })
 
   it('Detect ISO date in a version of the file, then update format in next version', async function () {
