a33f09ec30e669158ccedbfcf8acb66352b4aeda
Alban Mouton <alban.mouton@gmail.com>
2024-11-27 10:29:15 +0100

    feat: handle interaction query param for map preview (#207)
    mostly useful in print mode

--- public/pages/embed/dataset/_id/map.vue
+++ public/pages/embed/dataset/_id/map.vue
@@ -3,6 +3,7 @@
     :height-margin="0"
     :show-select="false"
     :single-item="$route.query._id_eq ?? $route.query[`_d_${dataset.id}__id_eq`]"
+    :no-interaction="noInteraction"
   />
 </template>
 
@@ -11,7 +12,10 @@ import { mapState } from 'vuex'
 
 export default {
   computed: {
-    ...mapState('dataset', ['dataset'])
+    ...mapState('dataset', ['dataset']),
+    noInteraction () {
+      return this.$route.query.interaction === 'false' || this.$route.query.interaction === '0'
+    }
   }
 }
 </script>
