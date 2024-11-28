7974c40f8f517506b23a27b29bb770dd4b404016
Alban Mouton <alban.mouton@gmail.com>
2024-11-28 14:04:40 +0100

    feat: embedded form supports some update cases based on various filters

--- public/pages/embed/dataset/_id/form.vue
+++ public/pages/embed/dataset/_id/form.vue
@@ -14,12 +14,14 @@
     </v-alert>
     <template v-else-if="dataset || jsonSchema">
       <v-form
+        v-if="line"
         ref="editLineForm"
         v-model="valid"
       >
         <dataset-edit-line-form
           v-model="line"
           :own-lines="ownLinesMode"
+          :readonly-cols="Object.keys(queryContext.data)"
           @onFileUpload="onFileUpload"
         />
         <v-row>
@@ -52,7 +54,8 @@ export default {
     valid: false,
     saving: false,
     sent: false,
-    line: {},
+    existingLine: null,
+    line: null,
     file: null
   }),
   computed: {
@@ -61,6 +64,45 @@ export default {
     ...mapGetters('dataset', ['resourceUrl']),
     ownLinesMode () {
       return (this.dataset && this.dataset.rest && this.dataset.rest.lineOwnership) || (!this.dataset && !!this.jsonSchema)
+    },
+    queryContext () {
+      const filters = {}
+      const data = {}
+      for (const [key, value] of Object.entries(this.$route.query)) {
+        if (key.endsWith('_eq')) {
+          if (key.startsWith('_c_')) {
+            const conceptId = key.slice(3, -3)
+            const property = this.dataset.schema.find(p => p['x-concept']?.id === conceptId)
+            if (!property) {
+              console.error(`property matching concept ${conceptId} not found`)
+            } else {
+              filters[key] = value
+              data[property.key] = value
+            }
+          } else if (key.startsWith('_d_')) {
+            const datasetPrefix = `_d_${this.dataset.id}_`
+            if (key.startsWith(datasetPrefix)) {
+              const propertyKey = key.replace(datasetPrefix, '').slice(0, -3)
+              filters[propertyKey + '_eq'] = value
+              data[propertyKey] = value
+            }
+          } else {
+            const propertyKey = key.slice(-3)
+            filters[key] = value
+            data[propertyKey] = value
+          }
+        }
+      }
+      return { filters, data }
+    }
+  },
+  async created () {
+    if (Object.keys(this.queryContext.data).length) {
+      const existingLines = await this.$axios.$get(`${this.resourceUrl}/lines`, { params: this.queryContext.filters })
+      this.existingLine = existingLines.results[0]
+      this.line = this.existingLine ? { ...this.existingLine } : { ...this.queryContext.data }
+    } else {
+      this.line = {}
     }
   },
   methods: {
@@ -72,9 +114,15 @@ export default {
       this.saving = true
       await new Promise(resolve => setTimeout(resolve, 100))
       if (this.ownLinesMode) {
-        await this.$axios.$post(`${this.resourceUrl}/own/${this.activeAccount.type}:${this.activeAccount.id}/lines`, this.line)
+        const line = { ...this.line }
+        if (this.existingLine) line._id = this.existingLine._id
+        await this.$axios.$post(`${this.resourceUrl}/own/${this.activeAccount.type}:${this.activeAccount.id}/lines`, line)
       } else {
-        await this.$store.dispatch('dataset/saveLine', { line: this.line, file: this.file })
+        if (this.existingLine) {
+          await this.$store.dispatch('dataset/saveLine', { line: this.line, file: this.file, id: this.existingLine._id })
+        } else {
+          await this.$store.dispatch('dataset/saveLine', { line: this.line, file: this.file })
+        }
       }
       this.saving = false
       this.sent = true
