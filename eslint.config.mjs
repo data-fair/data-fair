c31a12cdd688419d8040ae4a16db7669d5b276f3
Alban Mouton <alban.mouton@gmail.com>
2025-01-30 16:20:12 +0100

    chore: reactivate quality checks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import neostandard from 'neostandard'
 import dfLibRecommended from '@data-fair/lib-utils/eslint/recommended.js'
 
 export default [
-  { ignores: ['ui/*', '**/.type/', 'dev/*', 'node_modules/*', 'test/*'] },
+  { ignores: ['ui/*', '**/.type/', 'dev/*', 'node_modules/*', 'test/*', 'doc/*'] },
   ...dfLibRecommended,
   ...neostandard({ ts: true })
 ]
