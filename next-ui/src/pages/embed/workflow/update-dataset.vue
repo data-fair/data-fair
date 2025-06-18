df12704d39c60fd0cec0f13e35fc465fe022a736
Alban Mouton <alban.mouton@gmail.com>
2025-06-18 09:41:59 +0200

    fix: implicit owner filter in update-dataset workflow

--- next-ui/src/pages/embed/workflow/update-dataset.vue
+++ next-ui/src/pages/embed/workflow/update-dataset.vue
@@ -9,11 +9,19 @@
 
 <script lang="ts" setup>
 const route = useRoute<'/embed/workflow/update-dataset'>()
+const { account } = useSessionAuthenticated()
 
 const updated = useStringSearchParam('updated')
+const owner = useStringSearchParam('owner')
+const ownerFilter = computed(() => {
+  if (owner.value) return owner.value
+  let ownerFilter = `${account.value.type}:${account.value.id}`
+  if (account.value.department) ownerFilter += ':' + account.value.department
+  return ownerFilter
+})
 
 const datasetParams = computed(() => ({
   publicationSites: route.query.publicationSite as string | undefined,
-  owner: route.query.owner as string | undefined
+  owner: ownerFilter.value
 }))
 </script>
