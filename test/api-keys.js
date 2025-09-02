c7eef6506be86734b19bfeb7d01531cd8995c7ba
Alban Mouton <alban.mouton@gmail.com>
2025-09-02 10:47:42 +0200

    feat: api keys benefit from email based permission (#267)

--- test/api-keys.js
+++ test/api-keys.js
@@ -19,6 +19,7 @@ describe('API keys', function () {
     assert.ok(key1)
     const key2 = res.data.apiKeys[1].clearKey
     assert.ok(key2)
+    assert.equal(res.data.email, 'dmeadus0@answers.com')
 
     // Right scope
     const axKey1 = await global.ax.builder(undefined, undefined, undefined, undefined, { headers: { 'x-apiKey': key1 } })
@@ -33,6 +34,14 @@ describe('API keys', function () {
     assert.equal(dataset.status, 'finalized')
     assert.equal(dataset.owner.type, 'user')
     assert.equal(dataset.owner.id, 'dmeadus0')
+
+    // API key should react to permission granted through user email
+    const orgDataset = await testUtils.sendDataset('datasets/dataset1.csv', global.ax.hlalonde3Org)
+    await assert.rejects(axKey2.get('/api/v1/datasets/' + orgDataset.id + '/lines'), { status: 403 })
+    await global.ax.hlalonde3Org.put('/api/v1/datasets/' + orgDataset.id + '/permissions', [
+      { type: 'user', email: 'dmeadus0@answers.com', classes: ['read'] }
+    ])
+    await axKey2.get('/api/v1/datasets/' + orgDataset.id + '/lines')
   })
 
   it('Create and use an organization level api key', async function () {
