d322ee803401f149ed20af263d949e2f4aa698c6
Alban Mouton <alban.mouton@gmail.com>
2025-09-08 14:53:40 +0200

    chore: missing wsEmitter init in some workers

--- api/src/workers/files-processor/index.ts
+++ api/src/workers/files-processor/index.ts
@@ -1,21 +1,25 @@
 import mongo from '#mongo'
 import type { FileDataset } from '#types'
 import config from '#config'
+import * as wsEmitter from '@data-fair/lib-node/ws-emitter.js'
 
 export const analyzeCsv = async function (dataset: FileDataset) {
   await mongo.connect(true)
+  await wsEmitter.init(mongo.db)
   const analyzeCsv = await import('./analyze-csv.ts')
   await analyzeCsv.default(dataset)
 }
 
 export const analyzeGeojson = async function (dataset: FileDataset) {
   await mongo.connect(true)
+  await wsEmitter.init(mongo.db)
   const analyzeGeojson = await import('./analyze-geojson.ts')
   await analyzeGeojson.default(dataset)
 }
 
 export const normalizeFile = async function (dataset: FileDataset) {
   await mongo.connect(true)
+  await wsEmitter.init(mongo.db)
   const normalize = await import('./normalize.ts')
   await normalize.default(dataset)
 }
