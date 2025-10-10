782efbc6c95d5101d41d5f50e2421b2501c6a726
Alban Mouton <alban.mouton@gmail.com>
2025-10-10 11:22:05 +0200

    feat: chose separator when downloading csv file (#279)

---
+++ next-ui/src/components/dataset/dataset-download-csv-options.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-list-item>
+    <v-select
+      v-model="csvSep"
+      :items="[',', ';']"
+      density="compact"
+      hide-details
+      :label="t('separator')"
+      template
+      style="width: 120px"
+      class="mt-1"
+      variant="plain"
+    />
+    <template #append>
+      <v-btn
+        :href="href"
+        variant="text"
+        color="primary"
+        @click="$emit('click')"
+      >
+        valider
+      </v-btn>
+    </template>
+  </v-list-item>
+</template>
+
+<i18n lang="yaml">
+fr:
+  separator: Séparateur
+  validate: valider
+en:
+  separator: Separator
+  validate: validate
+</i18n>
+
+<script setup lang="ts">
+defineProps({ href: { type: String, default: null } })
+defineEmits(['click'])
+const csvSep = defineModel('csvSep', { type: String, required: true })
+
+const { t } = useI18n()
+
+</script>
