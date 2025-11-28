d34a6a97ad3e7ed8bf0c513349f83afb172864e9
Alban Mouton <alban.mouton@gmail.com>
2025-11-28 14:55:53 +0100

    fix: missing service prefix when calling site api

--- api/src/misc/utils/site.ts
+++ api/src/misc/utils/site.ts
@@ -3,7 +3,7 @@ import memoize from 'memoizee'
 import axios from '@data-fair/lib-node/axios.js'
 
 export const getSiteHashes = memoize(async (siteUrl: string) => {
-  return axios.get<{ publicInfo: string, themeCss: string }>(config.privateDirectoryUrl + '/api/sites/_hashes', { headers: { 'x-forwarded-host': new URL(siteUrl).host } })
+  return axios.get<{ publicInfo: string, themeCss: string }>(config.privateDirectoryUrl + '/simple-directory/api/sites/_hashes', { headers: { 'x-forwarded-host': new URL(siteUrl).host } })
     .then(r => ({ THEME_CSS_HASH: r.data.publicInfo + '/', PUBLIC_SITE_INFO_HASH: r.data.themeCss + '/' }))
 }, {
   profileName: 'getSiteHashes',
