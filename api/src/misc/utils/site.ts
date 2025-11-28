eefc10f1fb0292153ed8a1479259a6c340c2e713
Alban Mouton <alban.mouton@gmail.com>
2025-11-28 15:06:53 +0100

    fix: missing x-forwarded headers when calling site api

--- api/src/misc/utils/site.ts
+++ api/src/misc/utils/site.ts
@@ -3,7 +3,14 @@ import memoize from 'memoizee'
 import axios from '@data-fair/lib-node/axios.js'
 
 export const getSiteHashes = memoize(async (siteUrl: string) => {
-  return axios.get<{ publicInfo: string, themeCss: string }>(config.privateDirectoryUrl + '/simple-directory/api/sites/_hashes', { headers: { 'x-forwarded-host': new URL(siteUrl).host } })
+  const url = new URL(siteUrl)
+  return axios.get<{ publicInfo: string, themeCss: string }>(config.privateDirectoryUrl + '/simple-directory/api/sites/_hashes', {
+    headers: {
+      'x-forwarded-proto': url.protocol.slice(0, -1),
+      'x-forwarded-host': url.hostname,
+      'x-forwarded-port': url.port
+    }
+  })
     .then(r => ({ THEME_CSS_HASH: r.data.publicInfo + '/', PUBLIC_SITE_INFO_HASH: r.data.themeCss + '/' }))
 }, {
   profileName: 'getSiteHashes',
