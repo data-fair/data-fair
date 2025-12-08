16844145e2c0e90484c1d2adb3b02cea2cb3f482
Alban Mouton <alban.mouton@gmail.com>
2025-12-08 17:25:51 +0100

    feat: work on left navigation menu and modules integrations (#301)

---
+++ ui/public/pages/admin/processings-plugins.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <nuxt-child />
+    <d-frame
+      id="processings"
+      :height="(windowHeight - 48) + 'px'"
+      src="/processings/admin/plugins/"
+      sync-params
+      resize="no"
+      :adapter.prop="stateChangeAdapter"
+      @notif="emitFrameNotif"
+    />
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import '@data-fair/frame/lib/d-frame.js'
+import createStateChangeAdapter from '@data-fair/frame/lib/vue-router/state-change-adapter'
+
+export default {
+  computed: {
+    stateChangeAdapter () {
+      return createStateChangeAdapter(this.$router)
+    }
+  },
+  methods: {
+    ...mapActions(['emitFrameNotif'])
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ ui/public/pages/metrics.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <nuxt-child />
+    <d-frame
+      id="metrics"
+      :height="(windowHeight - 48) + 'px'"
+      src="/metrics/embed/home"
+      sync-params
+      resize="no"
+      :adapter.prop="stateChangeAdapter"
+      @notif="emitFrameNotif"
+    />
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import '@data-fair/frame/lib/d-frame.js'
+import createStateChangeAdapter from '@data-fair/frame/lib/vue-router/state-change-adapter'
+
+export default {
+  computed: {
+    stateChangeAdapter () {
+      return createStateChangeAdapter(this.$router)
+    }
+  },
+  methods: {
+    ...mapActions(['emitFrameNotif'])
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
