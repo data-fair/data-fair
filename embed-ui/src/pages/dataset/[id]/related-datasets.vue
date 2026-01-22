371347fb2ecb67aa9057f64a3636dd0513e76762
Alban Mouton <alban.mouton@gmail.com>
2026-01-22 15:42:14 +0100

    feat: edit a dataset's related datasets (#309)

---
+++ embed-ui/src/pages/dataset/[id]/related-datasets.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <v-container
+    fluid
+    data-iframe-height
+    style="min-height:800px;"
+  >
+    <dataset-related-datasets
+      v-model="relatedDatasets"
+      @update:model-value="datasetStore.patchDataset.execute({relatedDatasets: relatedDatasets})"
+    />
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { provideDatasetStore } from '~/composables/dataset-store'
+
+useFrameContent()
+const route = useRoute<'/dataset/[id]/related-datasets'>()
+
+const relatedDatasets = ref<{ id: string, title: string }[]>([])
+const datasetStore = provideDatasetStore(route.params.id)
+watch(datasetStore.dataset, () => {
+  relatedDatasets.value = datasetStore.dataset.value?.relatedDatasets ?? []
+})
+</script>
