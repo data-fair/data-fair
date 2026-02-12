b1ee8763a2600287d8d98a871307c584f898e3b3
Alban Mouton <alban.mouton@gmail.com>
2026-02-12 17:34:49 +0100

    chore: some work on default colors

--- api/config/default.cjs
+++ api/config/default.cjs
@@ -77,19 +77,16 @@ module.exports = {
   theme: {
     dark: false,
     colors: {
-      primary: '#1E88E5', // blue.darken1
+      primary: '#1976D2', // blue-darken-2
       secondary: '#42A5F5', // blue.lighten1,
-      accent: '#FF9800', // orange.base
+      accent: '#AA00FF', // purple-accent-4
       error: '#FF5252', // red.accent2
       info: '#2196F3', // blue.base
       success: '#4CAF50', // green.base
       warning: '#E91E63', // pink.base
       admin: '#E53935' // red.darken1
     },
-    darkColors: {
-      primary: '#2196F3', // blue.base
-      success: '#00E676' // green.accent3
-    },
+    darkColors: { },
     cssUrl: null,
     cssText: ''
   },
