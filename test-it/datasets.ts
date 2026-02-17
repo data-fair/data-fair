cba56a11f76e9dce4830cf9f8243b5c40ac7acd6
Alban Mouton <alban.mouton@gmail.com>
2026-02-17 16:37:36 +0100

    refactor: migrate full datasets test to new runner (22 tests)

--- test-it/datasets.ts
+++ test-it/datasets.ts
@@ -183,4 +183,187 @@ describe('datasets', function () {
       await workers.hook('finalize/' + res.data.id)
     }
   })
+
+  it('Upload new dataset with pre-filled attributes', async function () {
+    const ax = dmeadusOrg
+    const form = new FormData()
+    form.append('title', 'A dataset with pre-filled title')
+    form.append('publications', '[{"catalog": "test", "status": "waiting"}]')
+    form.append('file', datasetFd, 'yet-a-dataset.csv')
+    const res = await ax.post('/api/v1/datasets', form, { headers: formHeaders(form) })
+    assert.equal(res.data.title, 'A dataset with pre-filled title')
+    const workers = await import('../api/src/workers/index.ts')
+    await workers.hook('finalize/' + res.data.id)
+  })
+
+  it('Upload new dataset with JSON body', async function () {
+    const ax = dmeadusOrg
+    const form = new FormData()
+    form.append('body', JSON.stringify({ title: 'A dataset with both file and JSON body' }))
+    form.append('file', datasetFd, 'yet-a-dataset.csv')
+    const res = await ax.post('/api/v1/datasets', form, { headers: formHeaders(form) })
+    assert.equal(res.data.title, 'A dataset with both file and JSON body')
+    const workers = await import('../api/src/workers/index.ts')
+    await workers.hook('finalize/' + res.data.id)
+  })
+
+  it('Upload new dataset with defined id', async function () {
+    const ax = dmeadus
+    let form = new FormData()
+    form.append('title', 'my title')
+    form.append('file', datasetFd, 'yet-a-dataset.csv')
+    let res = await ax.post('/api/v1/datasets/my-dataset-id', form, { headers: formHeaders(form) })
+    assert.equal(res.status, 201)
+    assert.equal(res.data.title, 'my title')
+    assert.equal(res.data.id, 'my-dataset-id')
+    const workers = await import('../api/src/workers/index.ts')
+    await workers.hook('finalize/my-dataset-id')
+    form = new FormData()
+    form.append('title', 'my other title')
+    form.append('file', datasetFd, 'yet-a-dataset.csv')
+    res = await ax.post('/api/v1/datasets/my-dataset-id', form, { headers: formHeaders(form) })
+    assert.equal(res.status, 200)
+    await workers.hook('finalize/my-dataset-id')
+  })
+
+  it('Reject some not URL friendly id', async function () {
+    const ax = dmeadus
+    const form = new FormData()
+    form.append('title', 'my title')
+    form.append('file', datasetFd, 'yet-a-dataset.csv')
+    await assert.rejects(ax.post('/api/v1/datasets/my dataset id', form, { headers: formHeaders(form) }), (err: any) => err.status === 400)
+  })
+
+  it('Reject some other pre-filled attributes', async function () {
+    const ax = dmeadusOrg
+    const form = new FormData()
+    form.append('id', 'pre-filling id is not possible')
+    form.append('file', datasetFd, 'yet-a-dataset.csv')
+    await assert.rejects(ax.post('/api/v1/datasets', form, { headers: formHeaders(form) }), (err: any) => err.status === 400)
+  })
+
+  it('Fail to upload new dataset when not authenticated', async function () {
+    const ax = anonymous
+    const form = new FormData()
+    await assert.rejects(ax.post('/api/v1/datasets', form, { headers: formHeaders(form) }), (err: any) => err.status === 401)
+  })
+
+  it('Upload dataset and update with different file name', async function () {
+    const ax = dmeadus
+    const form = new FormData()
+    form.append('file', datasetFd, 'dataset-name.csv')
+    let res = await ax.post('/api/v1/datasets/dataset-name', form, { headers: formHeaders(form) })
+    const workers = await import('../api/src/workers/index.ts')
+    await workers.hook('finalize/dataset-name')
+    res = await ax.get('/api/v1/limits/user/dmeadus0')
+    assert.ok(res.data.store_bytes.consumption > 150)
+    assert.ok(res.data.store_bytes.consumption < 300)
+
+    const form2 = new FormData()
+    form2.append('file', datasetFd, 'dataset-name2.csv')
+    res = await ax.put('/api/v1/datasets/dataset-name', form2, { headers: formHeaders(form2) })
+    const dataset = await workers.hook('finalize/dataset-name')
+    assert.equal(dataset.originalFile.name, 'dataset-name2.csv')
+    assert.equal(dataset.file.name, 'dataset-name2.csv')
+    assert.equal(dataset.updatedAt, dataset.dataUpdatedAt)
+    assert.notEqual(dataset.updatedAt, dataset.createdAt)
+    res = await ax.get('/api/v1/limits/user/dmeadus0')
+    assert.ok(res.data.store_bytes.consumption > 150)
+    assert.ok(res.data.store_bytes.consumption < 300)
+  })
+
+  it('Upload new dataset and detect types', async function () {
+    const ax = dmeadus
+    const dataset = await sendDataset('datasets/dataset-types.csv', ax)
+    assert.equal(dataset.schema[0].key, 'string1')
+    assert.equal(dataset.schema[0].type, 'string')
+
+    assert.equal(dataset.schema[1].key, 'bool1')
+    assert.equal(dataset.schema[1].type, 'boolean')
+
+    assert.equal(dataset.schema[2].key, 'bool2')
+    assert.equal(dataset.schema[2].type, 'boolean')
+
+    assert.equal(dataset.schema[3].key, 'bool3')
+    assert.equal(dataset.schema[3].type, 'boolean')
+
+    assert.equal(dataset.schema[4].key, 'string2')
+    assert.equal(dataset.schema[4].type, 'string')
+
+    assert.equal(dataset.schema[5].key, 'number1')
+    assert.equal(dataset.schema[5].type, 'integer')
+
+    assert.equal(dataset.schema[6].key, 'number2')
+    assert.equal(dataset.schema[6].type, 'integer')
+
+    assert.equal(dataset.schema[7].key, 'number3')
+    assert.equal(dataset.schema[7].type, 'number')
+  })
+
+  it('Upload dataset and update it\'s data and schema', async function () {
+    const ax = dmeadus
+    const form = new FormData()
+    form.append('file', datasetFd, 'dataset-name.csv')
+    let res = await ax.post('/api/v1/datasets/dataset-name', form, { headers: formHeaders(form) })
+    const workers = await import('../api/src/workers/index.ts')
+    await workers.hook('finalize/dataset-name')
+    res = await ax.get('/api/v1/datasets/dataset-name')
+    const schema = res.data.schema.filter((f: any) => !f['x-calculated'])
+    schema.forEach((f: any) => {
+      delete f.enum
+      delete f['x-cardinality']
+      f['x-transform'] = { type: 'string' }
+      f.type = 'string'
+      delete f.format
+    })
+    const form2 = new FormData()
+    form2.append('file', datasetFd, 'dataset-name.csv')
+    form2.append('schema', JSON.stringify(schema))
+    res = await ax.post('/api/v1/datasets/dataset-name', form2, { headers: formHeaders(form2) })
+    await workers.hook('finalize/dataset-name')
+    res = await ax.get('/api/v1/datasets/dataset-name')
+    assert.equal(res.data.schema.filter((f: any) => f['x-transform']).length, 6)
+  })
+
+  it('Sort datasets by title', async function () {
+    const ax = dmeadus
+
+    for (const title of ['aa', 'bb', 'àb', ' àb', '1a']) {
+      await ax.post('/api/v1/datasets', { isRest: true, title })
+    }
+
+    let res = await ax.get('/api/v1/datasets', { params: { select: 'title', raw: true, sort: 'title:1' } })
+    assert.deepEqual(res.data.results.map((d: any) => d.title), ['1a', 'aa', 'àb', 'àb', 'bb'])
+    res = await ax.get('/api/v1/datasets', { params: { select: 'id,title', raw: true, sort: 'title:-1' } })
+    assert.deepEqual(res.data.results.map((d: any) => d.title), ['bb', 'àb', 'àb', 'aa', '1a'])
+
+    await ax.patch('/api/v1/datasets/' + res.data.results[0].id, { slug: 'test-slug' })
+    await assert.rejects(ax.patch('/api/v1/datasets/' + res.data.results[1].id, { slug: 'test-slug' }), (error: any) => {
+      assert.equal(error.status, 400)
+      assert.ok(error.data.includes('Ce slug est déjà utilisé'))
+      return true
+    })
+    res = await ax.post('/api/v1/datasets', { isRest: true, title: 'test slug 2', slug: 'test-slug' })
+    assert.equal(res.data.slug, 'test-slug-2')
+  })
+
+  it('Upload new dataset and specify encoding', async function () {
+    const ax = dmeadus
+    const form = new FormData()
+    form.append('file', datasetFd, 'dataset1.csv')
+    form.append('file_encoding', 'ISO-8859-1')
+    const res = await ax.post('/api/v1/datasets', form, { headers: formHeaders(form) })
+    const workers = await import('../api/src/workers/index.ts')
+    let dataset = await workers.hook('finalize/' + res.data.id)
+    assert.equal(dataset.file.explicitEncoding, 'ISO-8859-1')
+    assert.equal(dataset.file.encoding, 'ISO-8859-1')
+
+    const form2 = new FormData()
+    form2.append('file', datasetFd, 'dataset1.csv')
+    form2.append('file_encoding', 'ISO-8859-2')
+    await ax.post('/api/v1/datasets/' + dataset.id, form2, { headers: formHeaders(form2) })
+    dataset = await workers.hook('finalize/' + dataset.id)
+    assert.equal(dataset.file.explicitEncoding, 'ISO-8859-2')
+    assert.equal(dataset.file.encoding, 'ISO-8859-2')
+  })
 })
