94012032023a8cef4daa49693f022762bb06c0a4
Alban Mouton <alban.mouton@gmail.com>
2026-03-20 20:08:25 +0100

    feat: render breadcrumbs in top navigation bar

--- ui/src/components/layout/layout-navigation-top.vue
+++ ui/src/components/layout/layout-navigation-top.vue
@@ -9,6 +9,12 @@
     <v-app-bar-title class="text-body-1">
       Data Fair
     </v-app-bar-title>
+    <v-breadcrumbs
+      v-if="showBreadcrumbs"
+      :items="breadcrumbItems"
+      density="compact"
+      class="pa-0"
+    />
     <v-spacer />
     <layout-notifications-queue
       v-if="$uiConfig.eventsIntegration && user"
@@ -20,9 +26,35 @@
 
 <script lang="ts" setup>
 import DfPersonalMenu from '@data-fair/lib-vuetify/personal-menu.vue'
+import { useDisplay } from 'vuetify'
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+import type { createBreadcrumbs } from '~/composables/use-breadcrumbs'
+
+const props = defineProps<{
+  breadcrumbs?: ReturnType<typeof createBreadcrumbs>
+}>()
 
 const drawer = defineModel<boolean>('drawer', { required: true })
 const { user } = useSession()
+const { mdAndUp } = useDisplay()
+const route = useRoute()
+
+const showBreadcrumbs = computed(() => {
+  if (!mdAndUp.value) return false
+  if (!props.breadcrumbs) return false
+  if (props.breadcrumbs.items.value.length === 0) return false
+  if (props.breadcrumbs.routeName.value !== (route.name as string)) return false
+  return true
+})
+
+const breadcrumbItems = computed(() => {
+  if (!props.breadcrumbs) return []
+  return props.breadcrumbs.items.value.map(item => ({
+    title: item.text,
+    to: item.to
+  }))
+})
 </script>
 
 <i18n lang="yaml">
