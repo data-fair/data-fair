880e08e43948c0fa057d4d724a47f4e3c8041665
Alban Mouton <alban.mouton@gmail.com>
2026-03-30 15:02:27 +0200

    fix: missing notification topics

--- ui/src/components/common/notifications-dialog.vue
+++ ui/src/components/common/notifications-dialog.vue
@@ -46,6 +46,7 @@ en:
 
 <script lang="ts" setup>
 import { mdiClose } from '@mdi/js'
+import settingsSchema from '../../../../api/types/settings/schema.js'
 
 const props = defineProps<{
   resource: { id: string, slug?: string, title: string, owner: { type: string, id: string, department?: string } }
@@ -59,12 +60,29 @@ const showDialog = ref(false)
 
 const eventsUrl = window.location.origin + '/events'
 
+const webhooksSchema = settingsSchema.properties.webhooks
+
 const iframeUrl = computed(() => {
-  const keys = `data-fair:${props.resourceType}-*:${props.resource.slug}`
-  const titles = props.resource.title.replace(/,/g, ' ')
+  const webhooks = webhooksSchema.items.properties.events.items.oneOf
+    .filter((item: any) => {
+      if (!item.const.startsWith(props.resourceType)) return false
+      if (item.const === 'dataset-dataset-created') return false
+      if (item.const === 'dataset-finalize-end') return false
+      if (item.const === 'application-application-created') return false
+      return true
+    })
+  const keysParam = webhooks.map((w: any) => `data-fair:${w.const}:${props.resource.slug}`).join(',')
+  const titlesParam = webhooks.map((w: any) => w.title.replace(/,/g, ' ')).join(',')
   const urlTemplate = `${$siteUrl}/data-fair/${props.resourceType}/${props.resource.id}`
   let sender = `${props.resource.owner.type}:${props.resource.owner.id}`
   if (props.resource.owner.department) sender += ':' + props.resource.owner.department
-  return `${eventsUrl}/embed/subscribe?key=${encodeURIComponent(keys)}&title=${encodeURIComponent(titles)}&url-template=${encodeURIComponent(urlTemplate)}&sender=${encodeURIComponent(sender)}&register=false`
+  const searchParams = new URLSearchParams({
+    key: keysParam,
+    title: titlesParam,
+    'url-template': urlTemplate,
+    sender,
+    register: 'false'
+  }).toString()
+  return `${eventsUrl}/embed/subscribe?${searchParams}`
 })
 </script>
