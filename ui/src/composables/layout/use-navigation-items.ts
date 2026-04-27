0e60abe48f41fcd398b4abb5046e5be90ce31430
Alban Mouton <alban.mouton@gmail.com>
2026-04-27 17:30:14 +0200

    fix: hide empty menu groupd

--- ui/src/composables/layout/use-navigation-items.ts
+++ ui/src/composables/layout/use-navigation-items.ts
@@ -98,7 +98,7 @@ export function useNavigationItems (options: { t: ComposerTranslation, locale: R
     if (canAdminDep.value && $uiConfig.portalsIntegration) {
       management.push({ to: '/portals', icon: mdiMonitorDashboard, title: t('portals') })
     }
-    groups.push({ key: 'management', title: t('group.management'), items: management })
+    if (management.length) groups.push({ key: 'management', title: t('group.management'), items: management })
 
     // Connectors group
     const connect: NavItem[] = []
