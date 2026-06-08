data-fair/data-fair

commit 2c14bb552c5c423f1219acf93745009d25cec84f

refactor: manage asynchronous worker tasks in threads (#269)

api/src/workers/short-processor/index.ts
@@ -1,0 +1,125 @@
+import config from '#config'
+import mongo from '#mongo'
+import es from '#es'
+import debugLib from 'debug'
+import { CronJob } from 'cron'
+import * as catalogs from '../../catalogs/plugins/index.js'
+import { internalError } from '@data-fair/lib-node/observer.js'
+import * as wsEmitter from '@data-fair/lib-node/ws-emitter.js'
+import type { Application, Dataset, DatasetInternal, RestDataset } from '#types'
+
+export const harvest = async function (catalog: any) {
+  await mongo.connect(true)
+  const debug = debugLib(`worker:catalog-harvester:${catalog.id}`)
+  const db = mongo.db
+  const date = new Date()
+
+  const patch = { autoUpdate: JSON.parse(JSON.stringify(catalog.autoUpdate || {})) }
+  patch.autoUpdate.lastUpdate = { date }
+
+  try {
+    await catalogs.updateAllHarvestedDatasets(catalog)
+  } catch (err: any) {
+    internalError('catalog-harvester', err)
+    patch.autoUpdate.lastUpdate.error = err.message
+  }
+
+  const job = new CronJob(config.catalogAutoUpdates.cron, () => {})
+  patch.autoUpdate.nextUpdate = job.nextDate().toISO()
+
+  await db.collection('catalogs').updateOne({ id: catalog.id }, { $set: patch })
+  debug('done')
+}
+
+export const publishApplication = async function (application: Application) {
+  await mongo.connect(true)
+  await wsEmitter.init(mongo.db)
+  return catalogs.processPublications('application', application)
+}
+
+export const publishDataset = async function (dataset: Dataset) {
+  await mongo.connect(true)
+  await wsEmitter.init(mongo.db)
+  return catalogs.processPublications('dataset', dataset)
+}
+
+export const renewApiKey = async function (dataset: DatasetInternal) {
+  await mongo.connect(true)
+  const datasetsService = await import('../../datasets/service.js')
+  const readApiKeyUtils = await import('../../datasets/utils/read-api-key.js')
+  const debug = debugLib(`worker:read-api-key-renewer:${dataset.id}`)
+
+  const patch: Partial<DatasetInternal> = { readApiKey: { ...dataset.readApiKey } }
+  patch._readApiKey = readApiKeyUtils.update(dataset.owner, patch.readApiKey, dataset._readApiKey)
+  debug('renewing readApiKey', patch)
+  await datasetsService.applyPatch(dataset, patch)
+  debug('done')
+}
+
+export const manageTTL = async function (dataset: RestDataset) {
+  await mongo.connect(true)
+  const restUtils = await import('../../datasets/utils/rest.ts')
+  return restUtils.applyTTL(dataset)
+}
+
+export const autoUpdate = async function (dataset: Dataset) {
+  await mongo.connect(true)
+  const draft = {
+    status: 'imported',
+    draftReason: {
+      key: 'file-updated' as const,
+      message: 'Nouveau fichier chargé sur un jeu de données existant',
+      validationMode: 'always' as const
+    }
+  }
+  await mongo.datasets.updateOne({ id: dataset.id }, { $set: { draft } })
+}
+
+export const errorRetry = async function (dataset: Dataset) {
+  await mongo.connect(true)
+  const propertyPrefix = dataset.draftReason ? 'draft.' : ''
+  const patch = {
+    $set: {
+      [propertyPrefix + 'status']: dataset.errorStatus
+    },
+    $unset: {
+      [propertyPrefix + 'errorStatus']: 1 as const,
+      [propertyPrefix + 'errorRetry']: 1 as const
+    }
+  }
+  await mongo.datasets.updateOne({ id: dataset.id }, patch)
+}
+
+export const autoUpdateExtension = async function (dataset: Dataset) {
+  await mongo.connect(true)
+  const extensions = [...dataset.extensions!]
+  for (const e of extensions) {
+    if (e.nextUpdate && e.nextUpdate < new Date().toISOString()) {
+      e.needsUpdate = true
+      delete e.nextUpdate
+    }
+  }
+  await mongo.datasets.updateOne({ id: dataset.id }, { $set: { extensions } })
+}
+
+export const finalize = async function (dataset: Dataset) {
+  await Promise.all([mongo.connect(true), es.connect()])
+  await wsEmitter.init(mongo.db)
+  const finalize = await import('./finalize.ts')
+  await finalize.default(dataset)
+}
+
+if (process.env.NODE_ENV === 'test') {
+  const nock = (await import('nock')).default
+  // fake catalog
+  nock('http://test-catalog.com')
+    .persist()
+    .get('/api/1/site/').reply(200, { title: 'My catalog' })
+    .get('/api/1/organizations/suggest/?q=koumoul').reply(200, [{ name: 'Koumoul' }])
+    .get('/api/1/datasets/suggest/?q=test').reply(200, [{ title: 'Test dataset' }])
+    .post('/api/1/datasets/').reply(201, { slug: 'my-dataset', page: 'http://test-catalog.com/datasets/my-dataset' })
+
+  // fake catalog
+  nock('http://not-a-catalog.com')
+    .persist()
+}

api/index.ts
@@ -9,25 +9,9 @@ app.run().then(app => {
   if (config.mode.includes('server')) {
     console.log('Web socket and HTTP server listening on http://localhost:%s\nExposed on %s', config.port, config.publicUrl)
   }
-  if (config.mode === 'task') {
-    // console.log(`Ran task ${process.argv[2]} / ${process.argv[3]} / ${process.argv[4]}`)
-    process.exit()
-  }
 }, err => {
-  if (config.mode === 'task') {
-    console.debug(`Failure in data-fair task ${process.argv[2]} / ${process.argv[3]} / ${process.argv[4]}`, err)
-    try {
-      // sometimes we miss details from the error response, safer to do this in a try/catch in case of circular data
-      console.debug('full error', JSON.stringify(err, null, 2))
-    } catch (err2) {
-      console.debug(err2)
-    }
-    console.error(err.message)
-    process.exit(-1)
-  } else {
-    internalError('df-process', err)
-    process.exit(-1)
-  }
+  internalError('df-process', err)
+  process.exit(-1)
 })
 
 process.on('SIGTERM', function onSigterm () {

api/src/workers/batch-processor/index.ts
@@ -1,0 +1,130 @@
+import mongo from '#mongo'
+import es from '#es'
+import config from '#config'
+import * as wsEmitter from '@data-fair/lib-node/ws-emitter.js'
+import type { FileDataset, RestDataset, Dataset } from '#types'
+
+export const extend = async function (dataset: Dataset) {
+  await Promise.all([mongo.connect(true), es.connect()])
+  await wsEmitter.init(mongo.db)
+  const extend = await import('./extend.ts')
+  await extend.default(dataset)
+}
+
+export const indexLines = async function (dataset: Dataset) {
+  await Promise.all([mongo.connect(true), es.connect()])
+  await wsEmitter.init(mongo.db)
+  const indexLines = await import('./index-lines.ts')
+  await indexLines.default(dataset)
+}
+
+export const validateFile = async function (dataset: FileDataset) {
+  await mongo.connect(true)
+  await wsEmitter.init(mongo.db)
+  const validateFile = await import('./validate-file.ts')
+  await validateFile.default(dataset)
+}
+
+export const exportRest = async function (dataset: RestDataset) {
+  await Promise.all([mongo.connect(true), es.connect()])
+  const exportRest = await import('./export-rest.ts')
+  await exportRest.default(dataset)
+}
+
+export type NockInfo = {
+  origin: string
+  method?: 'post' | 'get',
+  path: string,
+  reply: {
+    status?: number,
+    headers?: Record<string, string>
+    body: any
+  }
+}
+
+export const setNock = async function (nockInfo: NockInfo) {
+  const nock = (await import('nock')).default
+  nock(nockInfo.origin)[nockInfo.method ?? 'get'](nockInfo.path).reply(nockInfo.reply.status, nockInfo.reply.body, nockInfo.reply.headers)
+}
+
+export const setCoordsNock = async function (params: { nbInputs: number, latLon?: number, query?: string, multiply?: boolean, error?: string }) {
+  const nock = (await import('nock')).default
+  const assert = (await import('node:assert'))
+
+  nock('http://test.com').post('/geocoder/coords' + (params.query ?? '')).reply(200, (uri: string, requestBody: string) => {
+    const inputs = requestBody.trim().split('\n').map(line => JSON.parse(line))
+    assert.equal(inputs.length, params.nbInputs)
+    assert.deepEqual(Object.keys(inputs[0]), ['q', 'key'])
+    return inputs
+      .map((input, i) => {
+        if (params.error) return { key: input.key, error: params.error }
+        let latLon = params.latLon ?? 10
+        if (params.multiply) latLon = latLon * i
+        return { key: input.key, lat: latLon, lon: latLon, matchLevel: 'match' + i }
+      })
+      .map(line => JSON.stringify(line)).join('\n') + '\n'
+  })
+}
+
+export const setSireneNock = async function () {
+  const nock = (await import('nock')).default
+  const assert = (await import('node:assert'))
+  nock('http://test.com', { reqheaders: { 'x-apiKey': config.defaultRemoteKey.value } })
+  // /sirene/api/v1/etablissements_bulk?select=NOMEN_LONG%2Cbodacc.capital%2CTEFET%2Clocation.lat%2Clocation.lon'
+    .post('/sirene/etablissements_bulk?select=NOMEN_LONG%2Cbodacc.capital%2CTEFET%2Clocation.lat%2Clocation.lon')
+  // .query({ params: { select: 'NOMEN_LONG,bodacc.capital,TEFET,location.lat,location.lon' } })
+    .reply(200, (uri, requestBody) => {
+      const inputs = requestBody.trim().split('\n').map(JSON.parse)
+      assert.equal(inputs.length, 1)
+      assert.deepEqual(Object.keys(inputs[0]), ['siret', 'key'])
+      return JSON.stringify({
+        NOMEN_LONG: 'KOUMOUL',
+        'location.lon': '-2.748514',
+        'location.lat': '47.687173',
+        key: inputs[0].key
+      }) + '\n'
+    })
+}
+
+export const setSireneNock2 = async function () {
+  const nock = (await import('nock')).default
+  const assert = (await import('node:assert'))
+  nock('http://test.com', { reqheaders: { 'x-apiKey': config.defaultRemoteKey.value } })
+  // /sirene/api/v1/etablissements_bulk?select=NOMEN_LONG%2Cbodacc.capital%2CTEFET%2Clocation.lat%2Clocation.lon'
+    .post('/sirene/etablissements_bulk?select=siret,NOMEN_LONG%2Cbodacc.capital%2CTEFET%2Clocation.lat%2Clocation.lon')
+  // .query({ params: { select: 'NOMEN_LONG,bodacc.capital,TEFET,location.lat,location.lon' } })
+    .reply(200, (uri, requestBody) => {
+      const inputs = requestBody.trim().split('\n').map(JSON.parse)
+      assert.equal(inputs.length, 1)
+      assert.deepEqual(Object.keys(inputs[0]), ['siret', 'key'])
+      return JSON.stringify({
+        siret: '82898347800011',
+        NOMEN_LONG: 'KOUMOUL',
+        'location.lon': '-2.748514',
+        'location.lat': '47.687173',
+        key: inputs[0].key
+      }) + '\n'
+    })
+}
+
+export const setSireneNock3 = async function () {
+  const nock = (await import('nock')).default
+  const assert = (await import('node:assert'))
+  nock('http://test.com', { reqheaders: { 'x-apiKey': config.defaultRemoteKey.value } })
+  // /sirene/api/v1/etablissements_bulk?select=NOMEN_LONG%2Cbodacc.capital%2CTEFET%2Clocation.lat%2Clocation.lon'
+    .post('/sirene/etablissements_bulk?select=NOMEN_LONG%2Cbodacc.capital%2CTEFET')
+  // .query({ params: { select: 'NOMEN_LONG,bodacc.capital,TEFET,location.lat,location.lon' } })
+    .reply(200, (uri, requestBody) => {
+      const inputs = requestBody.trim().split('\n').map(JSON.parse)
+      assert.equal(inputs.length, 1)
+      assert.deepEqual(Object.keys(inputs[0]), ['siret', 'key'])
+      return JSON.stringify({
+        NOMEN_LONG: 'KOUMOUL',
+        key: inputs[0].key
+      }) + '\n'
+    })
+}
+
+export const setEnv = function ({ key, value }: { key: string, value: string | undefined }) {
+  process.env[key] = value
+}

api/src/workers/files-manager/index.ts
@@ -1,0 +1,29 @@
+import mongo from '#mongo'
+import es from '#es'
+import * as wsEmitter from '@data-fair/lib-node/ws-emitter.js'
+import type { Dataset } from '#types'
+import type { NockInfo } from '../batch-processor/index.ts'
+
+export const initialize = async function (dataset: Dataset) {
+  await Promise.all([mongo.connect(true), es.connect()])
+  await wsEmitter.init(mongo.db)
+  const initialize = await import('./initialize.ts')
+  await initialize.default(dataset)
+}
+
+export const downloadFile = async function (dataset: Dataset) {
+  await mongo.connect(true)
+  const downloadFile = await import('./download-file.ts')
+  await downloadFile.default(dataset)
+}
+
+export const storeFile = async function (dataset: Dataset) {
+  await mongo.connect(true)
+  const storeFile = await import('./store-file.ts')
+  await storeFile.default(dataset)
+}
+
+export const setNock = async function (nockInfo: NockInfo) {
+  const nock = (await import('nock')).default
+  nock(nockInfo.origin)[nockInfo.method ?? 'get'](nockInfo.path).reply(nockInfo.reply.status, nockInfo.reply.body, nockInfo.reply.headers)
+}

api/src/workers/files-processor/index.ts
@@ -1,0 +1,29 @@
+import mongo from '#mongo'
+import type { FileDataset } from '#types'
+import config from '#config'
+
+export const analyzeCsv = async function (dataset: FileDataset) {
+  await mongo.connect(true)
+  const analyzeCsv = await import('./analyze-csv.ts')
+  await analyzeCsv.default(dataset)
+}
+
+export const analyzeGeojson = async function (dataset: FileDataset) {
+  await mongo.connect(true)
+  const analyzeGeojson = await import('./analyze-geojson.ts')
+  await analyzeGeojson.default(dataset)
+}
+
+export const normalizeFile = async function (dataset: FileDataset) {
+  await mongo.connect(true)
+  const normalize = await import('./normalize.ts')
+  await normalize.default(dataset)
+}
+
+export const setEnv = function ({ key, value }: { key: string, value: string | undefined }) {
+  process.env[key] = value
+}
+
+export const setConfig = function ({ key, value }: { key: string, value: any }) {
+  config[key] = value
+}

api/src/workers/index.ts
@@ -1,0 +1,235 @@
+import type { ResourceType } from '#types'
+import { workers, tasks, pendingTasks } from './tasks.ts'
+import type { Task, WorkerId } from './types.ts'
+import mergeDraft from '../datasets/utils/merge-draft.js'
+import locks from '@data-fair/lib-node/locks.js'
+import mongo from '#mongo'
+import config from '#config'
+import debugLib from 'debug'
+import EventEmitter from 'node:events'
+import eventPromise from '@data-fair/lib-utils/event-promise.js'
+import * as journals from '../misc/utils/journals.ts'
+import * as ping from './ping.ts'
+import taskProgress from '../datasets/utils/task-progress.ts'
+import { Histogram } from 'prom-client'
+import { internalError } from '@data-fair/lib-node/observer.js'
+
+const debug = debugLib('workers')
+
+export { workers } from './tasks.ts'
+
+const workersTasksHistogram = new Histogram({
+  name: 'df_datasets_workers_tasks',
+  help: 'Number and duration in seconds of tasks run by the workers',
+  buckets: [0.1, 1, 10, 60, 600],
+  labelNames: ['task', 'status']
+})
+
+export const events = new EventEmitter()
+
+export const hook = async (key: string) => {
+  const { type, id } = await eventPromise<{ type: string, id: string }>(events, key, { timeout: 10000 })
+  const newResource = await mongo.db.collection(type).findOne({ id })
+  if (!newResource) throw new Error('resource not found after webhook hook')
+  return newResource
+}
+
+const getFreeWorkers = () => {
+  return (Object.keys(workers) as WorkerId[]).filter(key => !workers[key].needsDrain)
+}
+
+const getFreeTasks = (type: ResourceType) => {
+  const freeWorkers = getFreeWorkers()
+  return tasks[type].filter(t => freeWorkers.includes(t.worker))
+}
+
+export const queryNextResourceTask = async (_type?: string, _id?: string) => {
+  for (const type of ['catalogs', 'applications', 'datasets'] as ResourceType[]) {
+    if (_type && _type !== type) continue
+    const freeTasks = getFreeTasks(type)
+    const facets: any = {}
+    for (const task of freeTasks) {
+      const filter = task.mongoFilter()
+      // filters.push(filter)
+      // projection['_' + task.name] = { $cond: { if: filter, then: true, else: false } }
+      const facet = [
+        { $match: filter },
+        { $addFields: { _lockId: { $concat: [type, ':', '$id'] } } },
+        { $lookup: { from: 'locks', localField: '_lockId', foreignField: '_id', as: '_locks' } },
+        { $match: { _locks: { $size: 0 } } },
+        { $limit: 1 }
+      ]
+      if (_id) facet.unshift({ $match: { id: _id } })
+      facets[task.name] = facet
+    }
+    const results = await mongo.db.collection<any>(type).aggregate([{ $facet: facets }]).toArray().then(agg => agg[0])
+    for (const task of freeTasks) {
+      const resource = results[task.name][0]
+      if (resource) {
+        if (process.env.NODE_ENV === 'test') {
+          const resourceMatchedTasks = freeTasks.map(t => t.name).filter(t => results[t]?.some(r => r.id === resource.id))
+          if (resourceMatchedTasks.length > 1) events.emit('error', new Error('task selecion was not exclusive ' + JSON.stringify(resourceMatchedTasks)))
+        }
+
+        delete resource._locks
+        delete resource._lockId
+        // if there is something to be done in the draft mode of the dataset, it is prioritary
+        if (type === 'datasets' && resource.draft && resource.draft.status !== 'finalized' && (resource.draft.status !== 'error' || task.name === 'errorRetry')) {
+          mergeDraft(resource)
+        }
+        return { type, resource, task }
+      }
+    }
+  }
+}
+
+export const processResourceTask = async (type: ResourceType, resource: any, task: Task) => {
+  const id = resource.id
+  const ack = await locks.acquire(`${type}:${id}`, 'worker')
+  if (!ack) {
+    debug('failed to acquire lock for resource', type, id)
+    return
+  }
+  const taskFullKey = `${type}/${resource.id}/${task.name}`
+  pendingTasks[task.worker][`${type}/${resource.id}/${task.name}`] = resource.slug
+
+  const endTask = workersTasksHistogram.startTimer({ task: task.name })
+  let progress: ReturnType<typeof taskProgress> | null = null
+  if (task.eventsPrefix) {
+    const noStoreEvent = type === 'datasets'
+    await journals.log(type, resource, { type: task.eventsPrefix + '-start' } as any, noStoreEvent)
+    progress = taskProgress(resource.id, task.eventsPrefix)
+    await progress.start()
+  }
+  try {
+    debug(`run task ${task.name} - ${type} / ${resource.slug} (${resource.id})${resource.draftReason ? ' - draft' : ''}`)
+    await workers[task.worker].run(resource, { name: task.name })
+    endTask({ status: 'ok' })
+    debug(`finished task ${task.name} - ${type} / ${resource.slug} (${resource.id})${resource.draftReason ? ' - draft' : ''}`)
+
+    let finalTask = false
+    if (task.eventsPrefix) {
+      const newResource = await mongo.db.collection(type).findOne({ id: resource.id })
+      if (newResource) {
+        const noStoreEvent = type === 'datasets' && (task.eventsPrefix !== 'finalize' || !!resource._partialRestStatus)
+        if (resource.draftReason) {
+          await journals.log(type, mergeDraft({ ...newResource }), { type: task.eventsPrefix + '-end' } as any, noStoreEvent)
+        } else {
+          await journals.log(type, newResource as any, { type: task.eventsPrefix + '-end' } as any, noStoreEvent)
+        }
+        finalTask = task.eventsPrefix === 'finalize' ||
+          (task.eventsPrefix === 'validate' && resource.draftReason && !newResource.draft && newResource.status) // special case of cancelled draft
+      }
+      await progress?.end(false, finalTask)
+    }
+    events.emit(task.name + '/' + id, { type, id })
+  } catch (err: any) {
+    let errorMessage = (typeof err === 'string' ? err : (err.message as string)) || 'unknown error'
+
+    if (stopped) {
+      console.log('task failed while service was shutting down', errorMessage)
+      endTask({ status: 'interrupted' })
+      return
+    }
+
+    endTask({ status: 'error' })
+
+    console.warn(`failure in worker ${task.name} - ${type} / ${resource.slug} (${resource.id})`, errorMessage)
+
+    // some error are caused by bad input, we should not retry these
+    let retry = !errorMessage.startsWith('[noretry] ')
+    if (!retry) {
+      debug('error message started by [noretry]')
+      errorMessage = errorMessage.replace('[noretry] ', '')
+    }
+    if (retry) {
+      // if this is the second time we get this error, do not retry anymore
+      const hasErrorRetry = await journals.hasErrorRetry(resource, type)
+      debug('does the journal have a recent error-retry event', hasErrorRetry)
+      if (hasErrorRetry) {
+        debug('last log in journal was already a retry')
+        retry = false
+      }
+    }
+
+    await progress?.end(true)
+
+    const propertyPrefix = resource.draftReason ? 'draft.' : ''
+    const patch: any = {
+      $set: {
+        [propertyPrefix + 'status']: 'error',
+        [propertyPrefix + 'errorStatus']: resource.status
+      }
+    }
+    if (retry) {
+      await journals.log(type, resource, { type: 'error-retry', data: errorMessage } as any)
+      patch.$set[propertyPrefix + 'errorRetry'] = new Date((new Date()).getTime() + config.worker.errorRetryDelay).toISOString()
+    } else {
+      await journals.log(type, resource, { type: 'error', data: errorMessage } as any)
+      patch.$unset = { [propertyPrefix + 'errorRetry']: 1 }
+    }
+
+    await mongo.db.collection(type).updateOne({ id: resource.id }, patch)
+
+    events.emit('error', err)
+  } finally {
+    delete pendingTasks[task.worker][taskFullKey]
+    await locks.release(`${type}:${id}`)
+  }
+}
+
+let stopped = false
+
+export const init = async () => {
+  await ping.init()
+}
+
+export const loop = async () => {
+  debug('start workers loop')
+  let resolveWaitInterval: (() => void) | undefined
+
+  ping.listen(async (type, id) => {
+    const resourceTask = await queryNextResourceTask(type, id)
+    debug('fetch resourceTask from ping', type, id, resourceTask?.task.name)
+    if (resourceTask) {
+      processResourceTask(resourceTask.type, resourceTask.resource, resourceTask.task).catch(err => {
+        internalError('worker-task', err)
+      }).then(() => {
+        // at the end of task ignore the wait interval and loop immediately
+        if (resolveWaitInterval) resolveWaitInterval()
+      })
+    }
+  }).catch(err => internalError('worker-ping-listen', err))
+
+  while (true) {
+    if (stopped) break
+    let resourceTask = await queryNextResourceTask()
+    while (resourceTask) {
+      debug('work on resource', resourceTask.type, resourceTask.resource.id, resourceTask.task.name)
+      if (stopped) break
+      processResourceTask(resourceTask.type, resourceTask.resource, resourceTask.task).catch(err => {
+        internalError('worker-task', err)
+      }).then(() => {
+        // at the end of task ignore the wait interval and loop immediately
+        if (resolveWaitInterval) resolveWaitInterval()
+      })
+      resourceTask = await queryNextResourceTask()
+    }
+    debug('wait for ' + config.worker.interval)
+    await new Promise<void>(resolve => {
+      resolveWaitInterval = resolve
+      setTimeout(resolve, config.worker.interval)
+    })
+  }
+  debug('finished workers loop')
+}
+
+export const stop = async () => {
+  debug('stop workers loop')
+  stopped = true
+  await ping.stop()
+  for (const worker of Object.values(workers)) {
+    await worker.close()
+  }
+  debug('closed all workers')
+}

api/types/dataset/index.ts
@@ -1,6 +1,7 @@
 import type { Request as ExpressRequest } from 'express'
 import type { Dataset } from './.type/index.js'
 import { httpError, type Account } from '@data-fair/lib-express'
+import type { InitFrom } from '#doc/datasets/post-req/index.js'
 export * from './.type/index.js'
 
 type Action = 'create' | 'update' | 'delete' | 'patch' | 'createOrUpdate'
@@ -24,6 +25,17 @@ export const isFileDataset = (dataset: Dataset): dataset is FileDataset => {
 }
 
 export type DatasetExt = Dataset & { visibility: 'public' | 'private' | 'protected', public: 'boolean' }
+
+export type DatasetInternal = Dataset & {
+  loaded?: { attachments?: boolean, dataset?: Partial<FileDataset['originalFile']> } | null,
+  descendants?: string[]
+  descendantsFull?: DatasetInternal[]
+  initFrom?: (InitFrom & { role: string, department?: string }) | null
+  _partialRestStatus?: 'updated' | 'extended' | 'indexed'
+  validateDraft?: boolean
+  _newRestAttachments?: string[]
+  _readApiKey?: { current: string, previous: string }
+}
 
 export type DatasetLine = {
   _id: string,

api/types/index.ts
@@ -5,7 +5,7 @@ import type { BaseApp } from './base-app/index.js'
 export type { Application } from './application/index.js'
 export type { AppConfig } from './app-config/index.js'
 export type { BaseApp } from './base-app/index.js'
-export type { Dataset, DatasetExt, RestDataset, DatasetLine, DatasetLineAction, DatasetLineRevision, VirtualDataset, FileDataset, SchemaProperty } from './dataset/index.ts'
+export type { Dataset, DatasetExt, DatasetInternal, RestDataset, DatasetLine, DatasetLineAction, DatasetLineRevision, VirtualDataset, FileDataset, SchemaProperty } from './dataset/index.ts'
 export type { Event } from './event/index.js'
 export { type Settings, resolvedSchema as settingsSchema } from './settings/index.js'
 export type { Topic } from './topic/index.js'
@@ -53,3 +53,5 @@ export type RestActionsSummary = {
 }
 
 export type BaseAppWithContext = BaseApp & { disabled: string[] }
+
+export type Nullable<T> = { [K in keyof T]: T[K] | null }

commit ae3f3421131898abebd7fa37396f3b3c9bc5a435

fix: missing db connection in manageTTL worker
applyTTL iterates elasticsearch hits (iterHits -> es.client) to find the
expired lines to delete, but manageTTL only connected mongo. Same latent
bug class as validateFile, introduced in the #269 threaded-worker refactor
and masked because the shortProcessor thread is usually kept warm by
finalize (which connects es).

Co-Authored-By: Claude Opus 4.8 (1M context) <noreply@anthropic.com>

api/src/workers/short-processor/index.ts
@@ -20,7 +20,9 @@ export const renewApiKey = async function (dataset: DatasetInternal) {
 }
 
 export const manageTTL = async function (dataset: RestDataset) {
-  await mongo.connect(true)
+  // es.connect is required because applyTTL iterates elasticsearch hits (iterHits -> es.client)
+  // to find the expired lines to delete
+  await Promise.all([mongo.connect(true), es.connect()])
   const restUtils = await import('../../datasets/utils/rest.ts')
   return restUtils.applyTTL(dataset)
 }