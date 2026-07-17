98328d39253c07ca3bb9565fd2fda19ce6508c0f
Alban Mouton <alban.mouton@gmail.com>
2026-07-17 10:52:42 +0200

    chore: retire the listenWhenReady option, always open the port only when ready (#503)

--- api/config/default.cjs
+++ api/config/default.cjs
@@ -1,7 +1,6 @@
 module.exports = {
   mode: 'server_worker', // can be server_worker, server or worker
   port: 8080,
-  listenWhenReady: false,
   publicUrl: 'http://localhost:8080',
   oldPublicUrl: '', // special case when changing path of data-fair
   wsPublicUrl: 'ws://localhost:8080',
